dab9468d0ebb3551c6c5e9faaeaf2df8b2cab1d1
Rob Cameron <cameron@cs.sfu.ca>
2014-08-12 12:12:24 +0000

    Add Negated attribute for Names
    git-svn-id: http://parabix.costar.sfu.ca/svn/icGREP/icgrep-devel@3992 a82c6ad6-7e1c-4be0-8e68-26c050b43eaf

--- icgrep/re_name.cpp
+++ icgrep/re_name.cpp
@@ -3,12 +3,14 @@
 Name::Name()
 {
     mName = "";
+    mNegated = false;
     mType = Name::FixedLength;
 }
 
 Name::Name(std::string name)
 {
     mName = name;
+    mNegated = false;
     mType = Name::FixedLength;
 }
 
@@ -22,6 +24,16 @@ void Name::setName(std::string name)
 std::string Name::getName()
 {
     return mName;
+}
+
+bool Name::isNegated()
+{
+    return mNegated;
+}
+
+void Name::setNegated(bool is_negated)
+{
+    mNegated = is_negated;
 }
 
 void Name::setType(Name::Type type)
--- icgrep/re_name.h
+++ icgrep/re_name.h
@@ -13,11 +13,14 @@ public:
     Name(std::string name);
     void setName(std::string name);
     std::string getName();
+    void setNegated(bool is_negated);
+    bool isNegated();
     void setType(Type type);
     Type getType();
     ~Name();
 private:
     std::string mName;
+    bool mNegated;
     Type mType;
 };
 
