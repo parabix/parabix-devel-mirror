2735f9259e3ddcf7c398807b35963ccfba7b95bf
Rob Cameron <cameron@cs.sfu.ca>
2015-01-07 03:44:32 +0000

    Assertion header check-in
    git-svn-id: http://parabix.costar.sfu.ca/svn/icGREP/icgrep-devel@4407 a82c6ad6-7e1c-4be0-8e68-26c050b43eaf

---
+++ icgrep/re/re_assertion.h
@@ -0,0 +1,52 @@
+/*
+ *  Copyright (c) 2015 International Characters.
+ *  This software is licensed to the public under the Open Software License 3.0.
+ *  icgrep is a trademark of International Characters.
+ */
+
+#ifndef RE_ASSERTION_H
+#define RE_ASSERTION_H
+
+#include <re/re_re.h>
+
+namespace re {
+
+class Assertion : public RE {
+public:
+    static inline bool classof(const RE * re) {
+        return re->getClassTypeId() == ClassTypeId::Assertion;
+    }
+    static inline bool classof(const void *) {
+        return false;
+    }
+    enum class Kind {Lookbehind, Lookahead};
+    enum class Sense {Positive, Negative};
+
+    RE * getAsserted() const {return mAsserted;}
+    Assertion::Kind getKind() const {return mKind;}
+    Assertion::Sense getSense() const {return mSense;}
+    void setAsserted(RE * r) {mAsserted = r;}
+
+
+protected:
+    friend Assertion * makeAssertion(RE * asserted, Kind k, Sense s);
+    void* operator new (std::size_t size) noexcept {
+        return mAllocator.allocate(size);
+    }
+    Assertion(RE * r, Kind k, Sense s) : RE(ClassTypeId::Assertion), mAsserted(r), mKind(k), mSense(s) {}
+    virtual ~Assertion() {}
+
+private:
+    RE * mAsserted;
+    Kind mKind;
+    Sense mSense;
+};
+
+inline Assertion * makeAssertion(RE * asserted, Assertion::Kind k, Assertion::Sense s) {
+    return new Assertion(asserted, k, s);
+}
+
+}
+
+#endif // RE_ASSERTION_H
+
