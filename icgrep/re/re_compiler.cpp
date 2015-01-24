17b972f49a6a46890bc47404b8ac11e220ff77a1
Rob Cameron <cameron@cs.sfu.ca>
2015-01-24 21:01:17 +0000

    Use mPB instead of mCG in #ifdef'd code
    git-svn-id: http://parabix.costar.sfu.ca/svn/icGREP/icgrep-devel@4441 a82c6ad6-7e1c-4be0-8e68-26c050b43eaf

--- icgrep/re/re_compiler.cpp
+++ icgrep/re/re_compiler.cpp
@@ -80,7 +80,7 @@ void RE_Compiler::initializeRequiredStreams(cc::CC_Compiler & ccc) {
     PabloAST * CR = ccc.compileCC(makeCC(0x0D));
     PabloAST * LF_VT_FF_CR = ccc.compileCC(makeCC(0x0A, 0x0D));
 #ifndef USE_IF_FOR_CRLF
-    mCRLF = mCG.createAnd(mCG.createAdvance(CR, 1), mLineFeed);
+    mCRLF = mPB.createAnd(mPB.createAdvance(CR, 1), mLineFeed);
 #else
     PabloBlock & crb = PabloBlock::Create(mPB);
     PabloAST * cr1 = crb.createAdvance(CR, 1, "cr1");
@@ -93,19 +93,19 @@ void RE_Compiler::initializeRequiredStreams(cc::CC_Compiler & ccc) {
     PabloAST * u8pfx2 = ccc.compileCC(makeCC(0xC2, 0xDF));
     PabloAST * u8pfx3 = ccc.compileCC(makeCC(0xE0, 0xEF));
     PabloAST * u8pfx4 = ccc.compileCC(makeCC(0xF0, 0xF4));
-    PabloAST * u8pfx = mCG.createOr(mCG.createOr(u8pfx2, u8pfx3), u8pfx4);
+    PabloAST * u8pfx = mPB.createOr(mPB.createOr(u8pfx2, u8pfx3), u8pfx4);
     PabloAST * u8single = ccc.compileCC(makeCC(0x00, 0x7F));
-    mInitial = mCG.createOr(u8pfx, u8single, "initial");
+    mInitial = mPB.createOr(u8pfx, u8single, "initial");
 
-    PabloAST * u8scope32 = mCG.createAdvance(u8pfx3, 1);
-    PabloAST * u8scope42 = mCG.createAdvance(u8pfx4, 1);
-    PabloAST * u8scope43 = mCG.createAdvance(u8scope42, 1);
-    PabloAST * NEL = mCG.createAnd(mCG.createAdvance(ccc.compileCC(makeCC(0xC2)), 1), ccc.compileCC(makeCC(0x85)));
-    PabloAST * E2_80 = mCG.createAnd(mCG.createAdvance(ccc.compileCC(makeCC(0xE2)), 1), ccc.compileCC(makeCC(0x80)));
-    PabloAST * LS_PS = mCG.createAnd(mCG.createAdvance(E2_80, 1), ccc.compileCC(makeCC(0xA8,0xA9)));
-    PabloAST * LB_chars = mCG.createOr(LF_VT_FF_CR, mCG.createOr(NEL, LS_PS));
-    mNonFinal = mCG.createAssign(nonfinal, mCG.createOr(mCG.createOr(u8pfx, u8scope32), mCG.createOr(u8scope42, u8scope43)));
-    mUnicodeLineBreak = mCG.createAnd(LB_chars, mCG.createNot(mCRLF));  // count the CR, but not CRLF
+    PabloAST * u8scope32 = mPB.createAdvance(u8pfx3, 1);
+    PabloAST * u8scope42 = mPB.createAdvance(u8pfx4, 1);
+    PabloAST * u8scope43 = mPB.createAdvance(u8scope42, 1);
+    PabloAST * NEL = mPB.createAnd(mPB.createAdvance(ccc.compileCC(makeCC(0xC2)), 1), ccc.compileCC(makeCC(0x85)));
+    PabloAST * E2_80 = mPB.createAnd(mPB.createAdvance(ccc.compileCC(makeCC(0xE2)), 1), ccc.compileCC(makeCC(0x80)));
+    PabloAST * LS_PS = mPB.createAnd(mPB.createAdvance(E2_80, 1), ccc.compileCC(makeCC(0xA8,0xA9)));
+    PabloAST * LB_chars = mPB.createOr(LF_VT_FF_CR, mPB.createOr(NEL, LS_PS));
+    mNonFinal = mPB.createAssign(nonfinal, mPB.createOr(mPB.createOr(u8pfx, u8scope32), mPB.createOr(u8scope42, u8scope43)));
+    mUnicodeLineBreak = mPB.createAnd(LB_chars, mPB.createNot(mCRLF));  // count the CR, but not CRLF
 #endif
 
 #ifdef USE_IF_FOR_NONFINAL
