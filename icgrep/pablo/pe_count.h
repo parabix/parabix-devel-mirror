4893b6bb53a0160e8ba4bb8ac890fce48f3ab332
Rob Cameron <cameron@cs.sfu.ca>
2015-08-07 22:17:04 +0000

    add Pablo count operation - not yet functional
    git-svn-id: http://parabix.costar.sfu.ca/svn/icGREP/icgrep-devel@4719 a82c6ad6-7e1c-4be0-8e68-26c050b43eaf

---
+++ icgrep/pablo/pe_count.h
@@ -0,0 +1,49 @@
+/*
+ *  Copyright (c) 2015 International Characters.
+ *  This software is licensed to the public under the Open Software License 3.0.
+ *  icgrep is a trademark of International Characters.
+ */
+
+#ifndef PE_COUNT_H
+#define PE_COUNT_H
+
+#include <pablo/pabloAST.h>
+
+namespace pablo {
+
+class Count : public Statement {
+    friend class PabloBlock;
+public:
+    static inline bool classof(const PabloAST * e) {
+        return e->getClassTypeId() == ClassTypeId::Advance;
+    }
+    static inline bool classof(const void *) {
+        return false;
+    }
+    virtual ~Count() {
+    }
+    inline PabloAST * getExpr() const {
+        return getOperand(0);
+    }
+    inline void setGlobalCountIndex(const unsigned idx) {
+        globalCountIndex = idx;
+    }
+    inline unsigned getGlobalCountIndex() const {
+        return globalCountIndex;
+    }
+protected:
+    Count(PabloAST * expr, String * name)
+    : Statement(ClassTypeId::Count, {expr}, name)
+    {
+
+    }
+private:
+    unsigned globalCountIndex;
+};
+
+}
+
+#endif // PE_COUNT_H
+
+
+
