3c5f1015853a02d1aee9d092c572caa88829a542
Rob Cameron <cameron@cs.sfu.ca>
2015-11-30 16:32:25 +0000

    Bug fix for mmap of empty file
    git-svn-id: http://parabix.costar.sfu.ca/svn/icGREP/icgrep-devel@4883 a82c6ad6-7e1c-4be0-8e68-26c050b43eaf

--- icgrep/do_grep.cpp
+++ icgrep/do_grep.cpp
@@ -210,13 +210,18 @@ void GrepExecutor::doGrep(const std::string & fileName) {
 
     mFileSize = file_size(file);
     mapped_file mFile;
-    try {
-        mFile.open(mFileName, mapped_file::priv, mFileSize, 0);
-    } catch (std::ios_base::failure e) {
-        std::cerr << "Error: Boost mmap " << e.what() << std::endl;
-        return;
+    if (mFileSize == 0) {
+        mFileBuffer = nullptr;
     }
-    mFileBuffer = mFile.data();
+    else {
+        try {
+            mFile.open(mFileName, mapped_file::priv, mFileSize, 0);
+        } catch (std::ios_base::failure e) {
+            std::cerr << "Error: Boost mmap of " << mFileName << ": " << e.what() << std::endl;
+            return;
+        }
+        mFileBuffer = mFile.data();
+    }
 #else
     struct stat infile_sb;
     const int fdSrc = open(mFileName.c_str(), O_RDONLY);
@@ -234,15 +239,22 @@ void GrepExecutor::doGrep(const std::string & fileName) {
         return;
     }
     mFileSize = infile_sb.st_size;
-    mFileBuffer = (char *) mmap(NULL, mFileSize, PROT_READ, MAP_PRIVATE, fdSrc, 0);
-    if (mFileBuffer == MAP_FAILED) {
-        if (errno ==  ENOMEM) {
-            std::cerr << "Error:  mmap of " << mFileName << " failed: out of memory\n";
+    if (mFileSize == 0) {
+        mFileBuffer = nullptr;
+    }
+    else {
+        mFileBuffer = (char *) mmap(NULL, mFileSize, PROT_READ, MAP_PRIVATE, fdSrc, 0);
+        if (mFileBuffer == MAP_FAILED) {
+            if (errno ==  ENOMEM) {
+                std::cerr << "Error:  mmap of " << mFileName << " failed: out of memory\n";
+                close (fdSrc);
+            }
+            else {
+                std::cerr << "Error: mmap of " << mFileName << " failed with errno " << errno << ". Skipped.\n";
+                close (fdSrc);
+            }
+            return;
         }
-        else {
-            std::cerr << "Error: mmap of " << mFileName << " failed with errno " << errno << ". Skipped.\n";
-        }
-        return;
     }
 #endif
     size_t segment = 0;
