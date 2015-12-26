4c23282c3e134d6689264f77700d0c0c6c44feaa
Rob Cameron <cameron@cs.sfu.ca>
2015-12-26 21:04:40 +0000

    SSE specialized IDISA functions
    git-svn-id: http://parabix.costar.sfu.ca/svn/icGREP/icgrep-devel@4902 a82c6ad6-7e1c-4be0-8e68-26c050b43eaf

---
+++ icgrep/IDISA/idisa_sse_builder.h
@@ -0,0 +1,41 @@
+#ifndef IDISA_SSE_BUILDER_H
+#define IDISA_SSE_BUILDER_H
+
+/*
+ *  Copyright (c) 2015 International Characters.
+ *  This software is licensed to the public under the Open Software License 3.0.
+ *  icgrep is a trademark of International Characters.
+ */
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Constant.h>
+#include <llvm/IR/Type.h>
+#include <llvm/IR/Value.h>
+#include <IDISA/idisa_builder.h>
+
+using namespace llvm;
+
+namespace IDISA {
+
+class IDISA_SSE_Builder : public IDISA_Builder {
+public:
+  
+    IDISA_SSE_Builder(Module * m, Type * bitBlockType) : IDISA_Builder(m, bitBlockType) {
+    }
+    ~IDISA_SSE_Builder() {};
+
+};
+
+class IDISA_SSE2_Builder : public IDISA_SSE_Builder {
+public:
+  
+    IDISA_SSE2_Builder(Module * m, Type * bitBlockType) : IDISA_SSE_Builder(m, bitBlockType) {
+    }
+    Value * hsimd_packh(unsigned fw, Value * a, Value * b) override;
+    Value * hsimd_packl(unsigned fw, Value * a, Value * b) override;
+    ~IDISA_SSE2_Builder() {};
+
+};
+
+}
+
+#endif // IDISA_SSE_BUILDER_H
