ed425004be02eae6b70cef897d69c55ab85bb8ef
Rob Cameron <cameron@cs.sfu.ca>
2016-06-09 20:43:42 +0000

    Stream set declaraations and buffers
    git-svn-id: http://parabix.costar.sfu.ca/svn/icGREP/icgrep-devel@5044 a82c6ad6-7e1c-4be0-8e68-26c050b43eaf

---
+++ icgrep/kernels/streamset.cpp
@@ -0,0 +1,39 @@
+/*
+ *  Copyright (c) 2016 International Characters.
+ *  This software is licensed to the public under the Open Software License 3.0.
+ */
+
+    
+#include <kernels/streamset.h>
+#include <vector>
+#include <IDISA/idisa_builder.h>
+#include <llvm/IR/Type.h>
+    
+using namespace kernel;
+
+llvm::Type * StreamSetType::getStreamSetBlockType(IDISA::IDISA_Builder * iBuilder) {
+    llvm::Type * streamType = mFieldWidth == 1 ? iBuilder->getBitBlockType() : ArrayType::get(iBuilder->getBitBlockType(), mFieldWidth);
+    return ArrayType::get(streamType, mStreamCount);
+}
+    
+llvm::Type * StreamSetBuffer::getStreamSetBlockType() {
+    return mStreamSetType.getStreamSetBlockType(iBuilder);
+}
+
+llvm::Value * StreamSetBuffer::allocateBuffer() {
+    if (mStreamSetBufferPtr == nullptr) {
+        llvm::Type * streamBufferType = getStreamSetBlockType();
+        if (mSegmentSize > 1) streamBufferType = ArrayType::get(getStreamSetBlockType(), mSegmentSize);
+        mStreamSetBufferPtr = iBuilder->CreateAlloca(streamBufferType);
+    }
+    return mStreamSetBufferPtr;
+}
+
+llvm::Value * StreamSetBuffer::getBlockPointer(llvm::Value * blockNo) {
+    if (mSegmentSize == 1) return mStreamSetBufferPtr;
+    if (mSegmentSize == 0) return
+        iBuilder->CreateGEP(getStreamSetBlockType(), mStreamSetBufferPtr, {blockNo});
+    Value * offset = iBuilder->CreateURem(blockNo, iBuilder->getInt32(mSegmentSize));
+    return iBuilder->CreateGEP(getStreamSetBlockType(), mStreamSetBufferPtr, {offset});
+}
+
--- icgrep/kernels/streamset.h
+++ icgrep/kernels/streamset.h
@@ -1,12 +1,52 @@
+/*
+ *  Copyright (c) 2016 International Characters.
+ *  This software is licensed to the public under the Open Software License 3.0.
+ */
+
 #ifndef STREAMSET_H
 #define STREAMSET_H
 
+#include <string>
+#include <vector>
+#include <IDISA/idisa_builder.h>
+#include <llvm/IR/Type.h>
+    
 namespace kernel {
 
+class StreamSetType {
+public:
+    StreamSetType(int count, int width) : mStreamCount(count), mFieldWidth(width) {}
+    int StreamCount() { return mStreamCount;}
+    int StreamFieldWidth() { return mFieldWidth;}
+    
+    llvm::Type * getStreamSetBlockType(IDISA::IDISA_Builder * iBuilder);
+    
+private:
+    int mStreamCount;
+    int mFieldWidth;
+};
 
+class StreamSetBuffer {
+public:
+    StreamSetBuffer(IDISA::IDISA_Builder * b, StreamSetType ss_type, unsigned SegmentSize) :
+    iBuilder(b), mStreamSetType(ss_type), mSegmentSize(SegmentSize), mStreamSetBufferPtr(nullptr) {}
 
+    llvm::Type * getStreamSetBlockType();
+    
+    llvm::Type * getStreamSetBufferType();
+    
+    void setStreamSetBuffer(llvm::Value * ptr) {mStreamSetBufferPtr = ptr;}
+    
+    llvm::Value * allocateBuffer();
+    
+    llvm::Value * getBlockPointer(llvm::Value * blockNo);
+private:
+    IDISA::IDISA_Builder * iBuilder;
+    StreamSetType mStreamSetType;
+    unsigned mSegmentSize;
+    llvm::Value * mStreamSetBufferPtr;
+    
 
-
+};
 }
-
 #endif // STREAMSET_H
