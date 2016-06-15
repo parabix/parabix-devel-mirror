00946e93e32cd8111eb468961dfbbb8289fd87e2
Rob Cameron <cameron@cs.sfu.ca>
2016-06-15 02:02:17 +0000

    scanmatch kernel using new infrastructure
    git-svn-id: http://parabix.costar.sfu.ca/svn/icGREP/icgrep-devel@5055 a82c6ad6-7e1c-4be0-8e68-26c050b43eaf

--- icgrep/kernels/scanmatchgen.h
+++ icgrep/kernels/scanmatchgen.h
@@ -5,7 +5,10 @@
 #ifndef SCANMATCHGEN_H
 #define SCANMATCHGEN_H
 
-namespace llvm { class Module; }
+#include "streamset.h"
+#include "interface.h"
+
+namespace llvm { class Module; class Function;}
 
 namespace IDISA { class IDISA_Builder; }
 
@@ -15,6 +18,29 @@ class KernelBuilder;
 
 void generateScanMatch(llvm::Module * m, IDISA::IDISA_Builder * iBuilder, unsigned scanWordBitWidth, KernelBuilder * kBuilder, bool isNameExpression);
 
+    
+    
+class scanMatchKernel : public KernelInterface {
+public:
+    scanMatchKernel(IDISA::IDISA_Builder * iBuilder, unsigned scanwordBitWidth, bool isNameExpression) :
+    KernelInterface(iBuilder, "scanMatch",
+                    {StreamSetBinding{StreamSetType(2, 1), "matchResults"}}, 
+                    {}, 
+                    {ScalarBinding{iBuilder->getInt8PtrTy(), "FileBuf"}, ScalarBinding{iBuilder->getInt64Ty(), "FileSize"}, ScalarBinding{iBuilder->getInt64Ty(), "FileIdx"}}, 
+                    {}, 
+                    {ScalarBinding{iBuilder->getInt64Ty(), "BlockNo"}, ScalarBinding{iBuilder->getInt64Ty(), "LineStart"}, ScalarBinding{iBuilder->getInt64Ty(), "LineNum"}}),
+
+    mScanwordBitWidth(scanwordBitWidth),
+    mIsNameExpression(isNameExpression) {}
+        
+    std::unique_ptr<llvm::Module> createKernelModule() override;
+
+private:
+    llvm::Function * generateScanWordRoutine(llvm::Module * m);
+        
+    unsigned mScanwordBitWidth;
+    bool mIsNameExpression;
+};
 }
 
 #endif // SCANMATCHGEN_H
