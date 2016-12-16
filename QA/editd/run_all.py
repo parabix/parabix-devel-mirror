f4803475f2bb69ebbc7387c110bc303ac66f0215
Dan Lin <lindanl@sfu.ca>
2016-12-16 21:13:10 +0000

    Fix directory issue for editd test.
    git-svn-id: http://parabix.costar.sfu.ca/svn/icGREP/icgrep-devel@5225 a82c6ad6-7e1c-4be0-8e68-26c050b43eaf

--- QA/editd/run_all.py
+++ QA/editd/run_all.py
@@ -1,11 +1,17 @@
 from subprocess import *
 from string import *
+import sys, os, optparse
 
 if __name__ == "__main__":
+	option_parser = optparse.OptionParser(usage='python %prog [options] <grep_executable>', version='1.0')
+	option_parser.add_option('-e', '--exec_dir', dest = 'exec_dir', type='string', default='.', help = 'executable directory')
+	options, args = option_parser.parse_args(sys.argv[1:])
+	print options.exec_dir
 	out_f = open('OutputFiles/reads2', 'w')
 	expected_f = open('ExpectedOutput/reads2', 'r')
-	call(['../../editd-build/editd', '-f', 'TestFiles/reads2', 'chr.fa', '-display'], stdout=out_f)
+	call([options.exec_dir+'/editd', '-f', 'TestFiles/reads2', 'chr.fa', '-display'], stdout=out_f)
 	if open('OutputFiles/reads2', 'r').read()==expected_f.read():
-		print 'Succeeded'
+		print 'Edit Distance test succeeded.'
 	else:
-		print 'Failed'	
+		print 'Edit Distance test failed.'	
+		exit(1)
