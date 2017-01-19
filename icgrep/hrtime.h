ff03f7dc25a705ff9e9b60bd1a3028de3da8d679
Nigel Medforth <nmedfort@sfu.ca>
2017-01-19 23:36:42 +0000

    Removed generate_predefined_ucd_functions.cpp. No longer used with the kernel system.
    git-svn-id: http://parabix.costar.sfu.ca/svn/icGREP/icgrep-devel@5268 a82c6ad6-7e1c-4be0-8e68-26c050b43eaf

--- icgrep/hrtime.h
+++ icgrep/hrtime.h
@@ -3,38 +3,6 @@
 
 //Downloaded from code.Google.com
 
-#include <stdio.h>
-#include <string.h>
-#include <assert.h>
-
-// get the number of CPU cycles per microsecond from Linux /proc filesystem
-// return < 0 on error
-inline double getMHZ(void) {
-  double mhz = -1;
-  char line[1024], *s, search_str[] = "cpu MHz";
-  FILE* fp;
-
-  // open proc/cpuinfo
-  if ((fp = fopen("/proc/cpuinfo", "r")) == nullptr)
-    return -1;
-
-  // ignore all lines until we reach MHz information
-  while (fgets(line, 1024, fp) != nullptr) {
-    if (strstr(line, search_str) != nullptr) {
-      // ignore all characters in line up to :
-      for (s = line; *s && (*s != ':'); ++s)
-        ;
-      // get MHz number
-      if (*s && (sscanf(s+1, "%lf", &mhz) == 1))
-        break;
-    }
-  }
-
-  if (fp != nullptr)
-    fclose(fp);
-  return mhz;
-}
-
 typedef uint64_t timestamp_t;
 
 // get the number of CPU cycles since startup using rdtsc instruction
@@ -53,14 +21,6 @@ timestamp_t ts;
 #ifdef _MSC_VER
   return __rdtsc();
 #endif
-}
-
-// get the elapsed time (in milliseconds) since startup
-inline double getElapsedTime() {
-  static double CPU_HZ = 0;
-  if (CPU_HZ == 0)
-    CPU_HZ = getMHZ() * 1000000;
-  return (read_cycle_counter() / CPU_HZ);
 }
 
 #endif // __HRTIME_H__
