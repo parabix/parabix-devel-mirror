117705df565da29854c400da977cb6901f1be6f6
Nigel Medforth <nmedfort@sfu.ca>
2017-02-16 22:04:57 +0000

    Attempt to diagnose / fix issue on jenkins server.
    git-svn-id: http://parabix.costar.sfu.ca/svn/icGREP/icgrep-devel@5323 a82c6ad6-7e1c-4be0-8e68-26c050b43eaf

--- icgrep/IR_Gen/idisa_target.cpp
+++ icgrep/IR_Gen/idisa_target.cpp
@@ -10,14 +10,29 @@
 #include <IR_Gen/idisa_i64_builder.h>
 #include <IR_Gen/idisa_nvptx_builder.h>
 #include <llvm/IR/Module.h>
+#ifndef NDEBUG
+#include <llvm/ADT/Triple.h>
+#endif
 
 namespace IDISA {
     
 IDISA_Builder * GetIDISA_Builder(llvm::Module * mod) {
+    if (LLVM_UNLIKELY(mod == nullptr)) {
+        report_fatal_error("GetIDISA_Builder: module cannot be null");
+    }
+    if (LLVM_LIKELY(mod->getTargetTriple().empty())) {
+        mod->setTargetTriple(llvm::sys::getProcessTriple());
+    }
     const bool hasAVX2 = AVX2_available();
-    DataLayout DL(mod);
+    DataLayout DL(mod);    
     Type * const intTy = DL.getIntPtrType(mod->getContext());
     const auto registerWidth = intTy->getIntegerBitWidth();
+    #ifndef NDEBUG
+    Triple T(mod->getTargetTriple());
+    if (LLVM_UNLIKELY((T.isArch16Bit() && registerWidth != 16) || (T.isArch32Bit() && registerWidth != 32) || (T.isArch64Bit() && registerWidth != 64))) {
+        report_fatal_error("GetIDISA_Builder: target triple '" + mod->getTargetTriple() + "' register width does not match data layout (" + std::to_string(registerWidth) + ")");
+    }
+    #endif
     if (LLVM_LIKELY(codegen::BlockSize == 0)) {  // No BlockSize override: use processor SIMD width
         codegen::BlockSize = hasAVX2 ? 256 : 128;
     }
