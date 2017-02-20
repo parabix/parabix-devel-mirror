e92ee15b0f10717d59dfa349f2cfaa8524c0cb27
Rob Cameron <cameron@cs.sfu.ca>
2017-02-20 18:20:48 +0000

    Swizzle Generator kernel - iniitial check-in
    git-svn-id: http://parabix.costar.sfu.ca/svn/icGREP/icgrep-devel@5332 a82c6ad6-7e1c-4be0-8e68-26c050b43eaf

---
+++ icgrep/kernels/swizzle.cpp
@@ -0,0 +1,81 @@
+/*
+ *  Copyright (c) 2017 International Characters.
+ *  This software is licensed to the public under the Open Software License 3.0.
+ */
+
+#include "swizzle.h"
+#include <IR_Gen/idisa_builder.h>
+#include <llvm/IR/Value.h>
+
+using namespace llvm;
+
+namespace kernel {
+
+SwizzleGenerator::SwizzleGenerator(IDISA::IDISA_Builder * iBuilder, unsigned bitStreamCount, unsigned outputSets, unsigned inputSets, unsigned fieldWidth)
+    : BlockOrientedKernel(iBuilder, "swizzle" + std::to_string(fieldWidth) + ":" + std::to_string(bitStreamCount), {}, {}, {}, {}, {})
+    , mBitStreamCount(bitStreamCount)
+    , mFieldWidth(fieldWidth)
+    , mSwizzleFactor(iBuilder->getBitBlockWidth() / fieldWidth)
+    , mInputSets(inputSets)
+    , mOutputSets(outputSets) {
+        assert((fieldWidth > 0) && (fieldWidth & (fieldWidth - 1) == 0) && "fieldWidth must be a power of 2");
+        assert(fieldWidth < iBuilder->getBitBlockWidth() && "fieldWidth must be less than the block width");
+        assert(mSwizzleFactor > 1 && "fieldWidth must be less than the block width");
+        unsigned inputStreamsPerSet = (bitStreamCount + inputSets - 1)/inputSets;
+        unsigned outputStreamsPerSet = (bitStreamCount + outputSets - 1)/outputSets;
+        // Maybe the following is unnecessary.
+        //assert(inputStreamsPerSet % swizzleFactor == 0 && "input sets must be an exact multiple of the swizzle factor");
+        assert(outputStreamsPerSet % swizzleFactor == 0 && "output sets must be an exact multiple of the swizzle factor");
+        for (unsigned i = 0; i < mInputSets; i++) {
+            mStreamSetInputs.push_back(Binding{iBuilder->getStreamSetTy(inputStreamsPerSet, 1), "inputGroup" + std::to_string(i)});
+        }
+        for (unsigned i = 0; i < mOutputSets; i++) {
+            mStreamSetOutputs.push_back(Binding{iBuilder->getStreamSetTy(outputStreamsPerSet, 1), "outputGroup" + std::to_string(i)});
+        }
+    }
+
+void SwizzleGenerator::generateDoBlockMethod() {
+        
+    // We may need a few passes depending on the swizzle factor
+    unsigned passes = std::log2(mSwizzleFactor);
+    
+    unsigned swizzleGroups = (mBitStreamCount + mSwizzleFactor - 1)/mSwizzleFactor;
+    unsigned inputStreamsPerSet = (mBitStreamCount + mInputSets - 1)/mInputSets;
+    unsigned outputStreamsPerSet = (mBitStreamCount + mOutputSets - 1)/mOutputSets;
+
+    for (unsigned grp = 0; grp < swizzleGroups; grp++) {
+        // First load all the data.
+        std::vector<Value *> sourceBlocks;        
+        std::vector<Value *> targetBlocks;        
+        for (unsigned i = 0; i < mSwizzleFactor; i++) {
+            unsigned streamNo = grp * mSwizzleFactor + i;
+            if (streamNo < mBitStreamCount) {
+                unsigned inputSetNo = streamNo / inputStreamsPerSet;
+                unsigned j = streamNo % inputStreamsPerSet;
+                sourceBlocks.push_back(loadInputStreamBlock("inputGroup" + std::to_string(inputSetNo), iBuilder->getInt32(j)));
+            }
+            else {
+                // Fill in the remaining logically required streams of the last swizzle group with null values.
+                sourceBlocks.push_back(Constant::getNullValue(iBuilder->getBitBlockType()));
+            }
+        }
+        // Now perform the swizzle passes.
+        for (unsigned p = 0; p < passes; p++) {
+            std::vector<Value *> targetBlocks;
+            for (unsigned i = 0; i < mSwizzleFactor/2; i++) {
+                targetBlocks.push_back(iBuilder->esimd_mergel(mFieldWidth, sourceBlocks[i], sourceBlocks[i+mSwizzleFactor/2]));
+                targetBlocks.push_back(iBuilder->esimd_mergeh(mFieldWidth, sourceBlocks[i], sourceBlocks[i+mSwizzleFactor/2]));
+            }
+            sourceBlocks = targetBlocks;
+        }
+        for (unsigned i = 0; i < mSwizzleFactor; i++) {
+            unsigned streamNo = grp * mSwizzleFactor + i;
+            unsigned outputSetNo = streamNo / outputStreamsPerSet;
+            unsigned j = streamNo % outputStreamsPerSet;
+            storeOutputStreamBlock("outputGroup" + std::to_string(outputSetNo), iBuilder->getInt32(j), sourceBlocks[i]);
+        }
+    }
+}
+
+
+}
---
+++ icgrep/kernels/swizzle.h
@@ -0,0 +1,70 @@
+/*
+ *  Copyright (c) 2017 International Characters.
+ *  This software is licensed to the public under the Open Software License 3.0.
+ */
+#ifndef SWIZZLE_H
+#define SWIZZLE_H
+
+#include "kernel.h"
+namespace IDISA { class IDISA_Builder; }
+
+
+// The SwizzleGenerator class creates a kernel that transforms a set of bit streams into a swizzled form.
+// In swizzled form, one "swizzle field" each from a set of streams are grouped together to be processed 
+// as a unit using SIMD operations.   For example, for a swizzle field width of 64 and a block size of 256,
+// 4 streams are swizzled together to be operated on as a group.  The ratio of the block size to the
+// swizzle field size is known as the swizzle factor, in this case 4.
+
+// Swizzled form is convenient for performing sequential operations on parallel sets of streams,
+// such as compression of each swizzle field by known counts, or stitching together known numbers
+// of bits from two different sources.
+
+// Any number of bit streams may be swizzled together.  However, the outputSets are always
+// grouped together in multiples of the swizzle factor.   If the bit stream count is not
+// an exact multiple of the swizzle factor, null streams (all zero bits) are added for
+// each swizzle group.
+
+// The input streams may come from any number of parallel input sets, each of the same size.
+// The number of inputSets defaults to 1.
+
+// The output streams may be separated in a number of output sets.  However, output streams
+// sets must be a multiple of the swizzle factor size.
+
+// For example: consider the following 4 streams (32 bits each)
+// Stream 1:   abcdef00 ghi00000 jk000000 lmnop000
+// Stream 2:   qrstuv00 wxy00000 z1000000 23456000
+// Stream 3:   ABCDEF00 GHI00000 JK000000 LMNOP000
+// Stream 4:   QRSTUV00 WZY00000 Z1000000 23456000
+//
+// The swizzled output using a field width of 8 produces the following swizzles.
+//
+// Swizzle 1:  abcdef00 qrstuv00 ABCDEF00 QRSTUV00
+// Swizzle 2:  ghi00000 wxy00000 GHI00000 WZY00000
+// Swizzle 3:  jk000000 z1000000 JK000000 Z1000000
+// Swizzle 4:  lmnop000 23456000 LMNOP000 23456000
+//
+// Now it might be convenient to all fields of swizzle 1 by 2, all fields of swizzle 2 by 5 and so on.
+//
+namespace kernel {
+
+class SwizzleGenerator : public BlockOrientedKernel {
+public:
+    
+    SwizzleGenerator(IDISA::IDISA_Builder * iBuilder, unsigned bitStreamCount, unsigned outputSets = 1, unsigned inputSets = 1, unsigned fieldWidth = 64);
+    
+protected:
+    
+    void generateDoBlockMethod() override;
+    
+private:
+    const unsigned mBitStreamCount;
+    const unsigned mFieldWidth;
+    const unsigned mSwizzleFactor;
+    const unsigned mInputSets;
+    const unsigned mOutputSets;
+};
+
+}
+    
+#endif
+
