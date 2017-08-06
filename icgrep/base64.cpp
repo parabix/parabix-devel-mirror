946d557be07f349e3158ca5aa57a7a3f271d8242
Dan Lin <lindanl@sfu.ca>
2017-08-06 20:20:25 +0000

    base64: Add task parallel for multiple input files.
    git-svn-id: http://parabix.costar.sfu.ca/svn/icGREP/icgrep-devel@5601 a82c6ad6-7e1c-4be0-8e68-26c050b43eaf

--- icgrep/base64.cpp
+++ icgrep/base64.cpp
@@ -22,6 +22,7 @@
 #include <boost/interprocess/anonymous_shared_memory.hpp>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <mutex>
 
 using namespace llvm;
 
@@ -32,6 +33,7 @@ static cl::list<std::string> inputFiles(cl::Positional, cl::desc("<input file ..
 
 static cl::opt<bool> mMapBuffering("mmap-buffering", cl::desc("Enable mmap buffering."), cl::cat(base64Options));
 static cl::opt<bool> memAlignBuffering("memalign-buffering", cl::desc("Enable posix_memalign buffering."), cl::cat(base64Options));
+static cl::opt<int> Threads("threads", cl::desc("Total number of threads."), cl::init(1));
 
 
 using namespace kernel;
@@ -76,12 +78,19 @@ void base64PipelineGen(ParabixDriver & pxDriver) {
     Kernel * radix64K = pxDriver.addKernelInstance(make_unique<radix64Kernel>(iBuilder));
     pxDriver.makeKernelCall(radix64K, {Expanded3_4Out}, {Radix64out});
     
-    StreamSetBuffer * Base64out = pxDriver.addBuffer(make_unique<DynamicBuffer>(iBuilder, iBuilder->getStreamSetTy(1, 8), bufferSize));
-    Kernel * base64K = pxDriver.addKernelInstance(make_unique<base64Kernel>(iBuilder));
-    pxDriver.makeKernelCall(base64K, {Radix64out}, {Base64out});
-    
-    Kernel * outK = pxDriver.addKernelInstance(make_unique<StdOutKernel>(iBuilder, 8));
-    pxDriver.makeKernelCall(outK, {Base64out}, {});
+    if (memAlignBuffering){
+        auto Base64out = pxDriver.addExternalBuffer(make_unique<ExternalBuffer>(iBuilder, iBuilder->getStreamSetTy(1, 8), outputStream));
+        Kernel * base64K = pxDriver.addKernelInstance(make_unique<base64Kernel>(iBuilder));
+        pxDriver.makeKernelCall(base64K, {Radix64out}, {Base64out});
+    }
+    else {
+        StreamSetBuffer * Base64out = pxDriver.addBuffer(make_unique<DynamicBuffer>(iBuilder, iBuilder->getStreamSetTy(1, 8), bufferSize));
+        Kernel * base64K = pxDriver.addKernelInstance(make_unique<base64Kernel>(iBuilder));
+        pxDriver.makeKernelCall(base64K, {Radix64out}, {Base64out});
+        
+        Kernel * outK = pxDriver.addKernelInstance(make_unique<StdOutKernel>(iBuilder, 8));
+        pxDriver.makeKernelCall(outK, {Base64out}, {});
+    }
     
     pxDriver.generatePipelineIR();
     pxDriver.deallocateBuffers();
@@ -89,7 +98,6 @@ void base64PipelineGen(ParabixDriver & pxDriver) {
 
     pxDriver.finalizeObject();
 }
-
 
 typedef void (*base64FunctionType)(const uint32_t fd, char * outputBuffer);
 
@@ -114,11 +122,16 @@ void base64(base64FunctionType fn_ptr, const std::string & fileName) {
         outputBuffer.advise(boost::interprocess::mapped_region::advice_sequential);
         fn_ptr(fd, static_cast<char*>(outputBuffer.get_address()));
     } else if (memAlignBuffering) {
+        unsigned inputSize = file_size(fd);
+        unsigned paddingSize = (inputSize % 3) ? (4 - (inputSize % 3)) : 0;
+        unsigned outputSize = inputSize * 4/3 + paddingSize;
+
         char * outputBuffer;
-        if (posix_memalign(reinterpret_cast<void **>(&outputBuffer), 32, 2 * file_size(fd))) {
+        if (posix_memalign(reinterpret_cast<void **>(&outputBuffer), 32, inputSize * 2)) {
             throw std::bad_alloc();
         }
         fn_ptr(fd, outputBuffer);
+        fwrite(outputBuffer, outputSize, 1, stdout);
         free(reinterpret_cast<void *>(outputBuffer));
     } else { /* No external output buffer */
         fn_ptr(fd, nullptr);
@@ -127,16 +140,92 @@ void base64(base64FunctionType fn_ptr, const std::string & fileName) {
     
 }
 
+std::mutex count_mutex;
+size_t fileCount;
+base64FunctionType fn_ptr;
+
+std::vector<char *> resultStrs;
+std::vector<int> filesizes;
+
+void *Base64ThreadFunction(void *args)
+{
+    size_t fileIdx;
+
+    count_mutex.lock();
+    fileIdx = fileCount;
+    fileCount++;
+    count_mutex.unlock();
+
+    while (fileIdx < inputFiles.size()) {
+        const int fd = open(inputFiles[fileIdx].c_str(), O_RDONLY);
+        if (LLVM_UNLIKELY(fd == -1)) {
+            std::cerr << "Error: cannot open " << inputFiles[fileIdx] << " for processing. Skipped.\n";
+            exit(-1);
+        }
+
+        char * outputBuffer;
+        if (posix_memalign(reinterpret_cast<void **>(&outputBuffer), 32, 2 * file_size(fd))) {
+            throw std::bad_alloc();
+        }
+    
+        fn_ptr(fd, outputBuffer);
+        resultStrs[fileIdx] = outputBuffer;
+        filesizes[fileIdx] = file_size(fd);
+
+        count_mutex.lock();
+        fileIdx = fileCount;
+        fileCount++;
+        count_mutex.unlock();
+    }
+
+    pthread_exit(nullptr);
+}
+
 int main(int argc, char *argv[]) {
     codegen::ParseCommandLineOptions(argc, argv, {&base64Options, codegen::codegen_flags()});
 
-    ParabixDriver pxDriver("base64");
-    base64PipelineGen(pxDriver);
-    auto main = reinterpret_cast<base64FunctionType>(pxDriver.getMain());
-
-    for (unsigned i = 0; i != inputFiles.size(); ++i) {
-        base64(main, inputFiles[i]);
+    if (Threads == 1) {
+        ParabixDriver pxDriver("base64");
+        base64PipelineGen(pxDriver);
+        fn_ptr = reinterpret_cast<base64FunctionType>(pxDriver.getMain());     
+        for (unsigned i = 0; i != inputFiles.size(); ++i) {
+            base64(fn_ptr, inputFiles[i]);
+        }
     }
+    else{
+        memAlignBuffering = true;
+        ParabixDriver pxDriver("base64");
+        base64PipelineGen(pxDriver);
+        fn_ptr = reinterpret_cast<base64FunctionType>(pxDriver.getMain());
+
+        fileCount = 0;
+        const unsigned n = inputFiles.size();
+        resultStrs.resize(n);
+        filesizes.resize(n);
+
+        const unsigned numOfThreads = Threads;
+        pthread_t threads[numOfThreads];
+
+        for(unsigned long i = 0; i < numOfThreads; ++i){
+            const int rc = pthread_create(&threads[i], NULL, Base64ThreadFunction, (void *)i);
+            if (rc) {
+                llvm::report_fatal_error("Failed to create thread: code " + std::to_string(rc));
+            }
+        }
+
+        for(unsigned i = 0; i < numOfThreads; ++i) {
+            void * status = nullptr;
+            const int rc = pthread_join(threads[i], &status);
+            if (rc) {
+                llvm::report_fatal_error("Failed to join thread: code " + std::to_string(rc));
+            }
+        }
+
+        for (unsigned i=0; i<resultStrs.size(); i++){
+            unsigned paddingSize = (filesizes[i] % 3) ? (4 - (filesizes[i] % 3)) : 0;
+            fwrite(resultStrs[i], filesizes[i] * 4/3 + paddingSize, 1, stdout);
+        }
+    }   
 
     return 0;
 }
