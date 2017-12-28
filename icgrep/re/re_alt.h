bbf1b3b3721f88de33edd13cf97028bdc25d9c54
Rob Cameron <cameron@cs.sfu.ca>
2017-12-28 02:01:51 +0000

    RE optimizations
    git-svn-id: http://parabix.costar.sfu.ca/svn/icGREP/icgrep-devel@5811 a82c6ad6-7e1c-4be0-8e68-26c050b43eaf

--- icgrep/re/re_alt.h
+++ icgrep/re/re_alt.h
@@ -8,7 +8,10 @@
 #define ALT_H
 
 #include "re_re.h"
-#include "re_cc.h"
+#include <re/re_cc.h>
+#include <re/re_seq.h>
+#include <re/re_rep.h>
+#include <re/printer_re.h>
 #include <llvm/Support/Casting.h>
 
 namespace re {
@@ -47,33 +50,55 @@ protected:
 inline Alt * makeAlt() {
     return new Alt();
 }
+    
+inline void add_CC(std::vector<CC *> & CCs_found, CC * cc) {
+    for (unsigned j = 0; j < CCs_found.size(); j++) {
+        if (CCs_found[j]->getAlphabet() == cc->getAlphabet()) {
+            CCs_found[j] = makeCC(CCs_found[j], cc);
+            return;
+        }
+    }
+    CCs_found.push_back(cc);
+}
 
 template<typename iterator>
 RE * makeAlt(iterator begin, iterator end) {
     Alt * newAlt = makeAlt();
-    CC * unionCC = nullptr;
+    bool nullableAltFound = false;
+    std::vector<CC *> CCs_found;  // CCs with possibly different alphabets
     for (auto i = begin; i != end; ++i) {
         if (CC * cc = llvm::dyn_cast<CC>(*i)) {
-            unionCC = unionCC ? makeCC(unionCC, cc) : cc;
+            add_CC(CCs_found, cc);
         } else if (const Alt * alt = llvm::dyn_cast<Alt>(*i)) {
             // We have an Alt to embed within the alt.  We extract the individual
             // elements to include within the new alt.   Note that recursive flattening
             // is not required, if the elements themselves were created with makeAlt.
             for (RE * a : *alt) {
                 if (CC * cc = llvm::dyn_cast<CC>(a)) {
-                    unionCC = unionCC ? makeCC(unionCC, cc) : cc;
+                    add_CC(CCs_found, cc);
+                } else if (isEmptySeq(a)) {
+                    nullableAltFound = true;
                 } else {
                     newAlt->push_back(a);
                 }
             }
-        }
-        else {
+        } else if (const Rep * rep = llvm::dyn_cast<Rep>(*i)) {
+            if (rep->getLB() == 0) {
+                nullableAltFound = true;
+                newAlt->push_back(makeRep(rep->getRE(), 1, rep->getUB()));
+            } else {
+                newAlt->push_back(*i);
+            }
+        } else if (isEmptySeq(*i)) {
+            nullableAltFound = true;
+        } else {
             newAlt->push_back(*i);
         }
     }
-    if (unionCC) {
-        newAlt->push_back(unionCC);
+    for (CC * cc : CCs_found) {
+        newAlt->push_back(cc);
     }
+    if (nullableAltFound) newAlt->push_back(makeSeq());
     if (newAlt->size() == 1) return newAlt->front();
     return newAlt;
 }
@@ -82,6 +107,10 @@ inline RE * makeAlt(RE::InitializerList list) {
     return makeAlt(list.begin(), list.end());
 }
 
+// An Alt with no members represent the empty set.
+inline bool isEmptySet(RE * r) {
+    return llvm::isa<Alt>(r) && llvm::cast<Alt>(r)->empty();
+}
 }
 
 #endif // ALT_H
