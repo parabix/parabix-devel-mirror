b2e7e787c48f7125321af94ddf3d2803e95f67ac
Xiangyu Wu <xwa163@sfu.ca>
2018-02-20 02:24:03 +0000

    Use random test file generator for character_deposit testing
    git-svn-id: http://parabix.costar.sfu.ca/svn/icGREP/icgrep-devel@5875 a82c6ad6-7e1c-4be0-8e68-26c050b43eaf

--- QA/character_deposit/character_deposit_test.py
+++ QA/character_deposit/character_deposit_test.py
@@ -2,49 +2,80 @@ import character_deposit
 import sys, subprocess, optparse, os, string, random
 import codecs
 import shutil
+import xml.etree.ElementTree as ET
+import testfile_generator
 
 global options
 global lz4d_program_under_test
 
 failure_count = 0
 
-def run_tests(test_dir, output_dir, python_output_dir):
+def generate_test_file(name, content):
+    if not content or len(content) == 0:
+        return
+    outfpath = os.path.join(options.datafile_dir, name)
+    outf = codecs.open(outfpath, encoding='utf-8', mode='w')
+    outf.write(content)
+    outf.close()
+
+
+def generateRandomTestFile(name, length, target_character):
+    content = testfile_generator.generate_text(length, target_character)
+    generate_test_file(name, content)
+
+
+def make_data_files(deposit_test_xml):
+    tree = ET.parse(deposit_test_xml)
+    root = tree.getroot()
+    for child in root:
+        if child.tag == 'random-testcase':
+            if "length" in child.attrib and "name" in child.attrib and "target-character" in child.attrib:
+                generateRandomTestFile(child.attrib["name"], int(child.attrib["length"]), child.attrib['target-character'])
+
+
+def run_tests(testfile_name, test_dir, output_dir, python_output_dir):
     global options
     global failure_count
 
-    test_files = os.listdir(test_dir)
-    for test_file in test_files:
-        if not test_file.endswith('.txt'):
-            continue
-        output_file = "out_" + test_file
+    tree = ET.parse(testfile_name)
+    root = tree.getroot()
+    for child in root:
+        if child.tag == 'random-testcase':
+            if "length" in child.attrib and "name" in child.attrib and "target-character" in child.attrib:
+                test_file = child.attrib["name"]
+                target_character = child.attrib['target-character']
 
-        test_file_full_path = os.path.join(test_dir, test_file)
-        output_file_full_path = os.path.join(output_dir, output_file)
-        python_output_file_full_path = os.path.join(python_output_dir, output_file)
+                output_file = "out_" + test_file
 
-        character_deposit_cmd = "%s b -buffer-segments=2 %s %s" % (lz4d_program_under_test, test_file_full_path, output_file_full_path)
+                test_file_full_path = os.path.join(test_dir, test_file)
+                output_file_full_path = os.path.join(output_dir, output_file)
+                python_output_file_full_path = os.path.join(python_output_dir, output_file)
 
-        if options.verbose:
-            print("Doing: " + character_deposit_cmd)
+                character_deposit_cmd = "%s b -buffer-segments=2 %s %s" % (
+                lz4d_program_under_test, test_file_full_path, output_file_full_path)
 
-        try:
-            character_deposit_out = subprocess.check_output(character_deposit_cmd.encode('utf-8'), cwd=options.exec_dir, shell=True)
-        except subprocess.CalledProcessError as e:
-            character_deposit_out = e.output
+                if options.verbose:
+                    print("Doing: " + character_deposit_cmd)
 
-        character_deposit.handle_deposit(test_file_full_path, python_output_file_full_path, 'b')
+                try:
+                    character_deposit_out = subprocess.check_output(character_deposit_cmd.encode('utf-8'),
+                                                                    cwd=options.exec_dir, shell=True)
+                except subprocess.CalledProcessError as e:
+                    character_deposit_out = e.output
 
-        with codecs.open(output_file_full_path, mode='rb') as inf:
-            output_content = inf.read()
-        with codecs.open(python_output_file_full_path, mode='rb') as inf:
-            expected_output_content = inf.read()
+                character_deposit.handle_deposit(test_file_full_path, python_output_file_full_path, 'b')
 
-        if output_content != expected_output_content:
-            print("Test failure: datafile {%s}" % test_file)
-            failure_count += 1
-        else:
-            if options.verbose:
-                print("Test success: datafile {%s}" % test_file)
+                with codecs.open(output_file_full_path, mode='rb') as inf:
+                    output_content = inf.read()
+                with codecs.open(python_output_file_full_path, mode='rb') as inf:
+                    expected_output_content = inf.read()
+
+                if output_content != expected_output_content:
+                    print("Test failure: datafile {%s}" % test_file)
+                    failure_count += 1
+                else:
+                    if options.verbose:
+                        print("Test success: datafile {%s}" % test_file)
 
     if failure_count > 0:
         print("Test Failure")
@@ -60,6 +91,9 @@ if __name__ == '__main__':
     option_parser.add_option('-d', '--datafile_dir',
                              dest='datafile_dir', type='string', default='testfiles',
                              help='directory for test files.')
+    option_parser.add_option('-t', '--testcases',
+                             dest='testcases', type='string', default='character_deposit_test.xml',
+                             help='character_deposit_test test case file (XML format).')
     option_parser.add_option('-e', '--exec_dir',
                              dest='exec_dir', type='string', default='.',
                              help='executable directory')
@@ -86,15 +120,21 @@ if __name__ == '__main__':
         shutil.rmtree(options.output_dir)
     os.mkdir(options.output_dir)
 
-
     if os.path.exists(options.python_output_dir):
         shutil.rmtree(options.python_output_dir)
     os.mkdir(options.python_output_dir)
 
+    test_dir = os.path.join(QA_dir, options.datafile_dir)
+    if os.path.exists(options.datafile_dir):
+        shutil.rmtree(options.datafile_dir)
+    os.mkdir(options.datafile_dir)
+
     lz4d_program_under_test = args[0]
 
-    test_dir = os.path.join(QA_dir, options.datafile_dir)
+    character_deposit_test_file_name = os.path.join(QA_dir, options.testcases)
+    make_data_files(character_deposit_test_file_name)
+
     output_dir = os.path.join(QA_dir, options.output_dir)
     python_output_dir = os.path.join(QA_dir, options.python_output_dir)
-    run_tests(test_dir, output_dir, python_output_dir)
+    run_tests(character_deposit_test_file_name, test_dir, output_dir, python_output_dir)
 
--- QA/character_deposit/testfile_generator.py
+++ QA/character_deposit/testfile_generator.py
@@ -2,18 +2,19 @@ import random
 import string
 
 
-def generate_text(num_of_group):
+def generate_text(num_of_group, target_character):
+    string_set = string.ascii_letters.replace(target_character, '')
+
     output_str = ''
     for i in range(num_of_group):
         group_size = random.randrange(1, 7)
         for j in range(group_size):
-            output_str += random.choice(string.ascii_uppercase)
+            output_str += random.choice(string_set)
         for j in range(group_size):
-            output_str += 'b'
+            output_str += target_character
 
     return output_str
 
-
 if __name__ == '__main__':
-    t = generate_text(20000)
+    t = generate_text(20000, 'b')
     print(t)
