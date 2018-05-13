aa5c647889b91a26c51d58e430b33ffe60df68f8
Xiangyu Wu <xwa163@sfu.ca>
2018-05-13 17:53:07 +0000

    Add bitstream deposit version of character_deposit pipeline
    git-svn-id: http://parabix.costar.sfu.ca/svn/icGREP/icgrep-devel@6033 a82c6ad6-7e1c-4be0-8e68-26c050b43eaf

--- icgrep/character_deposit.cpp
+++ icgrep/character_deposit.cpp
@@ -8,7 +8,6 @@
 #include <llvm/IR/Module.h>
 #include <llvm/IR/Function.h>
 #include <llvm/Linker/Linker.h>
-#include <llvm/Support/CommandLine.h>
 #include <llvm/Support/PrettyStackTrace.h>
 #include <llvm/Support/Signals.h>
 #include <llvm/Support/ManagedStatic.h>
@@ -30,9 +29,9 @@
 #include <kernels/p2s_kernel.h>
 #include <toolchain/cpudriver.h>
 #include <iostream>
-#include <fstream>
 #include <kernels/deletion.h>
 #include <kernels/pdep_kernel.h>
+#include <kernels/bitstream_pdep_kernel.h>
 
 namespace re { class CC; }
 
@@ -40,20 +39,102 @@ using namespace llvm;
 using namespace parabix;
 using namespace kernel;
 
-static cl::OptionCategory lz4dFlags("Command Flags", "deletion options");
-//static cl::opt<char> characterToBeExtract(cl::Positional, cl::desc("<character to be extracted>"), cl::Required, cl::cat(lz4dFlags));
-static cl::opt<char> characterToBeDeposit(cl::Positional, cl::desc("<character to be depositted>"), cl::Required, cl::cat(lz4dFlags));
-static cl::opt<std::string> inputFile(cl::Positional, cl::desc("<input file>"), cl::Required, cl::cat(lz4dFlags));
-static cl::opt<std::string> outputFile(cl::Positional, cl::desc("<output file>"), cl::Required, cl::cat(lz4dFlags));
+static cl::OptionCategory characterDepositFlags("Command Flags", "deletion options");
+static cl::opt<char> characterToBeDeposit(cl::Positional, cl::desc("<character to be depositted>"), cl::Required, cl::cat(characterDepositFlags));
+static cl::opt<std::string> inputFile(cl::Positional, cl::desc("<input file>"), cl::Required, cl::cat(characterDepositFlags));
+static cl::opt<std::string> outputFile(cl::Positional, cl::desc("<output file>"), cl::Required, cl::cat(characterDepositFlags));
+static cl::opt<bool> useSwizzledDeposit("swizzle-deposit", cl::desc("Use swizzle deletion"), cl::init(false), cl::cat(characterDepositFlags));
 
 
 typedef void (*MainFunctionType)(char * byte_data, size_t filesize);
 
 /*
  * Usage:
- *   character_deletion <character to be deleted> <input file name>
- * It will delete the character from the input file and then print the output to stdout
+ *     character_deposit <character to be deposit> <input file name> <output file name>
+ *
+ * Assume we have a input file with 6 character:
+ *     qbwbeb
+ * Then, the pipeline will first deleted all of character 'b' from input:
+ *     qbwbeb => qwe
+ * And then it will deposit the deleted result (qwe) to the original position of b:
+ *     qwe => \0 q \0 w \0 e (spaces are not included)
+ * and store the result to output file
  * */
+
+StreamSetBuffer * loadBasisBits(ParabixDriver & pxDriver, Value* inputStream, Value* fileSize, int bufferBlocks) {
+    auto & iBuilder = pxDriver.getBuilder();
+
+    StreamSetBuffer * ByteStream = pxDriver.addBuffer<SourceBuffer>(iBuilder, iBuilder->getStreamSetTy(1, 8));
+    StreamSetBuffer * BasisBits = pxDriver.addBuffer<CircularBuffer>(iBuilder, iBuilder->getStreamSetTy(8, 1), bufferBlocks);
+
+    kernel::Kernel * sourceK = pxDriver.addKernelInstance<MemorySourceKernel>(iBuilder);
+    sourceK->setInitialArguments({inputStream, fileSize});
+    pxDriver.makeKernelCall(sourceK, {}, {ByteStream});
+    Kernel * s2pk = pxDriver.addKernelInstance<S2PKernel>(iBuilder);
+    pxDriver.makeKernelCall(s2pk, {ByteStream}, {BasisBits});
+
+    return BasisBits;
+}
+
+StreamSetBuffer * generateSwizzledDeposit(ParabixDriver & pxDriver, StreamSetBuffer * BasisBits, int bufferBlocks) {
+    auto & iBuilder = pxDriver.getBuilder();
+
+    StreamSetBuffer * const CharacterMarkerBuffer = pxDriver.addBuffer<CircularBuffer>(iBuilder, iBuilder->getStreamSetTy(1, 1), bufferBlocks);
+    Kernel * ccK = pxDriver.addKernelInstance<ParabixCharacterClassKernelBuilder>(iBuilder, "extenders", std::vector<re::CC *>{re::makeCC(characterToBeDeposit)}, 8);
+    pxDriver.makeKernelCall(ccK, {BasisBits}, {CharacterMarkerBuffer});
+
+
+    StreamSetBuffer * u16Swizzle0 = pxDriver.addBuffer<CircularCopybackBuffer>(iBuilder, iBuilder->getStreamSetTy(4), bufferBlocks);
+    StreamSetBuffer * u16Swizzle1 = pxDriver.addBuffer<CircularCopybackBuffer>(iBuilder, iBuilder->getStreamSetTy(4), bufferBlocks);
+    Kernel * delK = pxDriver.addKernelInstance<SwizzledDeleteByPEXTkernel>(iBuilder, 8);
+    pxDriver.makeKernelCall(delK, {CharacterMarkerBuffer, BasisBits}, {u16Swizzle0, u16Swizzle1});
+
+    StreamSetBuffer * depositedSwizzle0 = pxDriver.addBuffer<DynamicBuffer>(iBuilder, iBuilder->getStreamSetTy(4), bufferBlocks, 1);
+    Kernel * pdep0K = pxDriver.addKernelInstance<PDEPkernel>(iBuilder, 4, "pdep0");
+    pxDriver.makeKernelCall(pdep0K, {CharacterMarkerBuffer, u16Swizzle0}, {depositedSwizzle0});
+
+
+    StreamSetBuffer * depositedSwizzle1 = pxDriver.addBuffer<DynamicBuffer>(iBuilder, iBuilder->getStreamSetTy(4), bufferBlocks, 1);
+    Kernel * pdep1K = pxDriver.addKernelInstance<PDEPkernel>(iBuilder, 4, "pdep1");
+    pxDriver.makeKernelCall(pdep1K, {CharacterMarkerBuffer, u16Swizzle1}, {depositedSwizzle1});
+
+    // Produce unswizzled bit streams
+    StreamSetBuffer * resultbits = pxDriver.addBuffer<CircularBuffer>(iBuilder, iBuilder->getStreamSetTy(8), bufferBlocks);
+    Kernel * unSwizzleK = pxDriver.addKernelInstance<SwizzleGenerator>(iBuilder, 8, 1, 2);
+
+    pxDriver.makeKernelCall(unSwizzleK, {depositedSwizzle0, depositedSwizzle1}, {resultbits});
+    return resultbits;
+}
+
+StreamSetBuffer * generateBitStreamDeposit(ParabixDriver & pxDriver, StreamSetBuffer * BasisBits, int bufferBlocks) {
+    auto & iBuilder = pxDriver.getBuilder();
+
+    StreamSetBuffer * const deletionMarker = pxDriver.addBuffer<CircularBuffer>(iBuilder, iBuilder->getStreamSetTy(1, 1), bufferBlocks);
+    Kernel * ccK1 = pxDriver.addKernelInstance<ParabixCharacterClassKernelBuilder>(iBuilder, "deletionMarker", std::vector<re::CC *>{re::subtractCC(re::makeByte(0, 255), re::makeCC(characterToBeDeposit))}, 8);
+    pxDriver.makeKernelCall(ccK1, {BasisBits}, {deletionMarker});
+
+    StreamSetBuffer * const depositMarker = pxDriver.addBuffer<CircularBuffer>(iBuilder, iBuilder->getStreamSetTy(1, 1), bufferBlocks);
+    Kernel * ccK2 = pxDriver.addKernelInstance<ParabixCharacterClassKernelBuilder>(iBuilder, "extenders", std::vector<re::CC *>{re::makeCC(characterToBeDeposit)}, 8);
+    pxDriver.makeKernelCall(ccK2, {BasisBits}, {depositMarker});
+
+    // Deletion
+    StreamSetBuffer * deletedBits = pxDriver.addBuffer<CircularBuffer>(iBuilder, iBuilder->getStreamSetTy(8), bufferBlocks);
+    StreamSetBuffer * deletionCounts = pxDriver.addBuffer<CircularBuffer>(iBuilder, iBuilder->getStreamSetTy(8), bufferBlocks);
+
+    Kernel * delK = pxDriver.addKernelInstance<PEXTFieldCompressKernel>(iBuilder, 64, 8);
+    pxDriver.makeKernelCall(delK, {BasisBits, deletionMarker}, {deletedBits, deletionCounts});
+
+    StreamSetBuffer * compressedBits = pxDriver.addBuffer<CircularBuffer>(iBuilder, iBuilder->getStreamSetTy(8), bufferBlocks);
+    Kernel * streamCompressionK = pxDriver.addKernelInstance<StreamCompressKernel>(iBuilder, 64, 8);
+    pxDriver.makeKernelCall(streamCompressionK, {deletedBits, deletionCounts}, {compressedBits});
+
+    // Deposit
+    StreamSetBuffer * depositedBits = pxDriver.addBuffer<DynamicBuffer>(iBuilder, iBuilder->getStreamSetTy(8), bufferBlocks, 1);
+    Kernel * pdepK = pxDriver.addKernelInstance<BitStreamPDEPKernel>(iBuilder, 8);
+    pxDriver.makeKernelCall(pdepK, {depositMarker, compressedBits}, {depositedBits});
+
+    return depositedBits;
+}
 
 int main(int argc, char *argv[]) {
     // This boilerplate provides convenient stack traces and clean LLVM exit
@@ -62,7 +143,7 @@ int main(int argc, char *argv[]) {
     sys::PrintStackTraceOnErrorSignal(argv[0]);
     llvm::PrettyStackTraceProgram X(argc, argv);
     llvm_shutdown_obj shutdown;
-    codegen::ParseCommandLineOptions(argc, argv, {&lz4dFlags, codegen::codegen_flags()});
+    codegen::ParseCommandLineOptions(argc, argv, {&characterDepositFlags, codegen::codegen_flags()});
 
     std::string fileName = inputFile;
 
@@ -78,7 +159,7 @@ int main(int argc, char *argv[]) {
 
     char *fileBuffer = const_cast<char *>(mappedFile.data());
 
-    const auto bufferSize = codegen::ThreadNum * codegen::SegmentSize;
+    const auto bufferBlocks = codegen::ThreadNum * codegen::SegmentSize;
 
     ParabixDriver pxDriver("character_deletion");
     auto & iBuilder = pxDriver.getBuilder();
@@ -99,43 +180,15 @@ int main(int argc, char *argv[]) {
 
     iBuilder->SetInsertPoint(BasicBlock::Create(M->getContext(), "entry", main, 0));
 
+    StreamSetBuffer * BasisBits = loadBasisBits(pxDriver, inputStream, fileSize, bufferBlocks);
+    StreamSetBuffer * resultbits = NULL;
+    if (useSwizzledDeposit) {
+        resultbits = generateSwizzledDeposit(pxDriver, BasisBits, bufferBlocks);
+    } else {
+        resultbits = generateBitStreamDeposit(pxDriver, BasisBits, bufferBlocks);
+    }
 
-    // GeneratePipeline
-    StreamSetBuffer * ByteStream = pxDriver.addBuffer<SourceBuffer>(iBuilder, iBuilder->getStreamSetTy(1, 8));
-    StreamSetBuffer * BasisBits = pxDriver.addBuffer<CircularBuffer>(iBuilder, iBuilder->getStreamSetTy(8, 1), bufferSize);
-
-    kernel::Kernel * sourceK = pxDriver.addKernelInstance<MemorySourceKernel>(iBuilder);
-    sourceK->setInitialArguments({inputStream, fileSize});
-    pxDriver.makeKernelCall(sourceK, {}, {ByteStream});
-    Kernel * s2pk = pxDriver.addKernelInstance<S2PKernel>(iBuilder);
-    pxDriver.makeKernelCall(s2pk, {ByteStream}, {BasisBits});
-
-    StreamSetBuffer * const CharacterMarkerBuffer = pxDriver.addBuffer<CircularBuffer>(iBuilder, iBuilder->getStreamSetTy(1, 1), bufferSize);
-    Kernel * ccK = pxDriver.addKernelInstance<ParabixCharacterClassKernelBuilder>(iBuilder, "extenders", std::vector<re::CC *>{re::makeCC(characterToBeDeposit)}, 8);
-    pxDriver.makeKernelCall(ccK, {BasisBits}, {CharacterMarkerBuffer});
-
-
-    StreamSetBuffer * u16Swizzle0 = pxDriver.addBuffer<CircularCopybackBuffer>(iBuilder, iBuilder->getStreamSetTy(4), bufferSize);
-    StreamSetBuffer * u16Swizzle1 = pxDriver.addBuffer<CircularCopybackBuffer>(iBuilder, iBuilder->getStreamSetTy(4), bufferSize);
-    Kernel * delK = pxDriver.addKernelInstance<SwizzledDeleteByPEXTkernel>(iBuilder, 8);
-    pxDriver.makeKernelCall(delK, {CharacterMarkerBuffer, BasisBits}, {u16Swizzle0, u16Swizzle1});
-
-    StreamSetBuffer * depositedSwizzle0 = pxDriver.addBuffer<DynamicBuffer>(iBuilder, iBuilder->getStreamSetTy(4), bufferSize, 1);
-    Kernel * pdep0K = pxDriver.addKernelInstance<PDEPkernel>(iBuilder, 4, "pdep0");
-    pxDriver.makeKernelCall(pdep0K, {CharacterMarkerBuffer, u16Swizzle0}, {depositedSwizzle0});
-
-
-    StreamSetBuffer * depositedSwizzle1 = pxDriver.addBuffer<DynamicBuffer>(iBuilder, iBuilder->getStreamSetTy(4), bufferSize, 1);
-    Kernel * pdep1K = pxDriver.addKernelInstance<PDEPkernel>(iBuilder, 4, "pdep1");
-    pxDriver.makeKernelCall(pdep1K, {CharacterMarkerBuffer, u16Swizzle1}, {depositedSwizzle1});
-
-    // Produce unswizzled bit streams
-    StreamSetBuffer * resultbits = pxDriver.addBuffer<CircularBuffer>(iBuilder, iBuilder->getStreamSetTy(8), bufferSize);
-    Kernel * unSwizzleK = pxDriver.addKernelInstance<SwizzleGenerator>(iBuilder, 8, 1, 2);
-
-    pxDriver.makeKernelCall(unSwizzleK, {depositedSwizzle0, depositedSwizzle1}, {resultbits});
-
-    StreamSetBuffer * const ResultBytes = pxDriver.addBuffer<CircularBuffer>(iBuilder, iBuilder->getStreamSetTy(1, 8), bufferSize);
+    StreamSetBuffer * const ResultBytes = pxDriver.addBuffer<CircularBuffer>(iBuilder, iBuilder->getStreamSetTy(1, 8), bufferBlocks);
     Kernel * p2sK = pxDriver.addKernelInstance<P2SKernel>(iBuilder);
     pxDriver.makeKernelCall(p2sK, {resultbits}, {ResultBytes});
 
