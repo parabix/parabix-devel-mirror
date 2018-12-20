6f00021b1b7ac03b3e8315e580317fe009213a95
Rob Cameron <cameron@cs.sfu.ca>
2018-12-20 15:55:35 +0000

    Use little-endian bit numbering for u8u16
    git-svn-id: http://parabix.costar.sfu.ca/svn/icGREP/icgrep-devel@6246 a82c6ad6-7e1c-4be0-8e68-26c050b43eaf

--- icgrep/u8u16.cpp
+++ icgrep/u8u16.cpp
@@ -92,32 +92,32 @@ void U8U16Kernel::generatePabloMethod() {
     Var * delmask = main.createVar("delmask", zeroes);
     Var * error_mask = main.createVar("error_mask", zeroes);
 
-    cc::Parabix_CC_Compiler ccc(getEntryScope(), u8_bits, cc::BitNumbering::BigEndian);
+    cc::Parabix_CC_Compiler ccc(getEntryScope(), u8_bits);
 
     // The logic for processing non-ASCII bytes will be embedded within an if-hierarchy.
     PabloAST * nonASCII = ccc.compileCC(re::makeByte(0x80, 0xFF));
 
     // Builder for the if statement handling all non-ASCII logic
     auto nAb = main.createScope();
-    // Bits 3 through 7 of a 2-byte prefix are data bits, needed to
+    // Bits 3 through LE0 of a 2-byte prefix are data bits, needed to
     // produce the UTF-16 code unit data ...,
-    PabloAST * bit3a1 = nAb.createAdvance(u8_bits[3], 1);
     PabloAST * bit4a1 = nAb.createAdvance(u8_bits[4], 1);
-    PabloAST * bit5a1 = nAb.createAdvance(u8_bits[5], 1);
-    PabloAST * bit6a1 = nAb.createAdvance(u8_bits[6], 1);
-    PabloAST * bit7a1 = nAb.createAdvance(u8_bits[7], 1);
+    PabloAST * bit3a1 = nAb.createAdvance(u8_bits[3], 1);
+    PabloAST * bit2a1 = nAb.createAdvance(u8_bits[2], 1);
+    PabloAST * bit1a1 = nAb.createAdvance(u8_bits[1], 1);
+    PabloAST * bit0a1 = nAb.createAdvance(u8_bits[0], 1);
 
     // Entry condition for 3 or 4 byte sequences: we have a prefix byte in the range 0xE0-0xFF.
     PabloAST * pfx34 = ccc.compileCC(re::makeByte(0xE0, 0xFF), nAb);
     // Builder for the if statement handling all logic for 3- and 4-byte sequences.
     auto p34b = nAb.createScope();
-    // Bits 4 through 7 of a 3-byte prefix are data bits.  They must be moved
+    // Bits LE3 through LE0 of a 3-byte prefix are data bits.  They must be moved
     // to the final position of the 3-byte sequence.
-    PabloAST * bit2a1 = p34b.createAdvance(u8_bits[2], 1);
-    PabloAST * bit4a2 = p34b.createAdvance(bit4a1, 1);
-    PabloAST * bit5a2 = p34b.createAdvance(bit5a1, 1);
-    PabloAST * bit6a2 = p34b.createAdvance(bit6a1, 1);
-    PabloAST * bit7a2 = p34b.createAdvance(bit7a1, 1);
+    PabloAST * bit5a1 = p34b.createAdvance(u8_bits[5], 1);
+    PabloAST * bit3a2 = p34b.createAdvance(bit3a1, 1);
+    PabloAST * bit2a2 = p34b.createAdvance(bit2a1, 1);
+    PabloAST * bit1a2 = p34b.createAdvance(bit1a1, 1);
+    PabloAST * bit0a2 = p34b.createAdvance(bit0a1, 1);
 
     Var * const u8scope32 = nAb.createVar("u8scope32", zeroes);
     Var * const u8scope33 = nAb.createVar("u8scope33", zeroes);
@@ -152,32 +152,32 @@ void U8U16Kernel::generatePabloMethod() {
 
     //  From the 4-byte sequence 11110abc 10defghi 10jklmno 10pqrstu,
     //  we must calculate the value abcde - 1 to produce the bit values
-    //  for u16_hi6, hi7, lo0, lo1 at the scope43 position.
-    Var * s43_lo0 = nAb.createVar("scope43_lo0", zeroes);
-    Var * s43_lo1 = nAb.createVar("scope43_lo1", zeroes);
-    Var * s43_hi6 = nAb.createVar("scope43_hi6", zeroes);
-    Var * s43_hi7 = nAb.createVar("scope43_hi7", zeroes);
+    //  for u16_hi1, hi0, lo7, lo6 at the scope43 position.
+    Var * s43_lo7 = nAb.createVar("scope43_lo7", zeroes);
+    Var * s43_lo6 = nAb.createVar("scope43_lo6", zeroes);
+    Var * s43_hi1 = nAb.createVar("scope43_hi1", zeroes);
+    Var * s43_hi0 = nAb.createVar("scope43_hi0", zeroes);
 
-    Var * s43_lo2 = main.createVar("scope43_lo2", zeroes);
-    Var * s43_lo3 = main.createVar("scope43_lo3", zeroes);
-    Var * s43_lo4 = main.createVar("scope43_lo4", zeroes);
     Var * s43_lo5 = main.createVar("scope43_lo5", zeroes);
-    Var * s43_lo6 = main.createVar("scope43_lo6", zeroes);
-    Var * s43_lo7 = main.createVar("scope43_lo7", zeroes);
+    Var * s43_lo4 = main.createVar("scope43_lo4", zeroes);
+    Var * s43_lo3 = main.createVar("scope43_lo3", zeroes);
+    Var * s43_lo2 = main.createVar("scope43_lo2", zeroes);
+    Var * s43_lo1 = main.createVar("scope43_lo1", zeroes);
+    Var * s43_lo0 = main.createVar("scope43_lo0", zeroes);
 
-    p4b.createAssign(s43_lo1, p4b.createAnd(u8scope43, p4b.createNot(bit3a1)));           // e - 1
-    p4b.createAssign(s43_lo0, p4b.createAnd(u8scope43, p4b.createXor(bit2a1, s43_lo1)));  // d - borrow
-    PabloAST * brw1 = p4b.createAnd(s43_lo1, p4b.createNot(bit2a1));
-    p4b.createAssign(s43_hi7, p4b.createAnd(u8scope43, p4b.createXor(bit7a2, brw1)));     // c - borrow
-    PabloAST * brw2 = p4b.createAnd(brw1, p4b.createNot(bit7a2));
-    p4b.createAssign(s43_hi6, p4b.createAnd(u8scope43, p4b.createXor(bit6a2, brw2)));     // b - borrow
+    p4b.createAssign(s43_lo6, p4b.createAnd(u8scope43, p4b.createNot(bit4a1)));           // e - 1
+    p4b.createAssign(s43_lo7, p4b.createAnd(u8scope43, p4b.createXor(bit5a1, s43_lo6)));  // d - borrow
+    PabloAST * brw1 = p4b.createAnd(s43_lo6, p4b.createNot(bit5a1));
+    p4b.createAssign(s43_hi0, p4b.createAnd(u8scope43, p4b.createXor(bit0a2, brw1)));     // c - borrow
+    PabloAST * brw2 = p4b.createAnd(brw1, p4b.createNot(bit0a2));
+    p4b.createAssign(s43_hi1, p4b.createAnd(u8scope43, p4b.createXor(bit1a2, brw2)));     // b - borrow
     //
-    p4b.createAssign(s43_lo2, p4b.createAnd(u8scope43, bit4a1));
-    p4b.createAssign(s43_lo3, p4b.createAnd(u8scope43, bit5a1));
-    p4b.createAssign(s43_lo4, p4b.createAnd(u8scope43, bit6a1));
-    p4b.createAssign(s43_lo5, p4b.createAnd(u8scope43, bit7a1));
-    p4b.createAssign(s43_lo6, p4b.createAnd(u8scope43, u8_bits[2]));
-    p4b.createAssign(s43_lo7, p4b.createAnd(u8scope43, u8_bits[3]));
+    p4b.createAssign(s43_lo5, p4b.createAnd(u8scope43, bit3a1));
+    p4b.createAssign(s43_lo4, p4b.createAnd(u8scope43, bit2a1));
+    p4b.createAssign(s43_lo3, p4b.createAnd(u8scope43, bit1a1));
+    p4b.createAssign(s43_lo2, p4b.createAnd(u8scope43, bit0a1));
+    p4b.createAssign(s43_lo1, p4b.createAnd(u8scope43, u8_bits[5]));
+    p4b.createAssign(s43_lo0, p4b.createAnd(u8scope43, u8_bits[4]));
     //
     //
     p34b.createIf(pfx4, p4b);
@@ -206,11 +206,11 @@ void U8U16Kernel::generatePabloMethod() {
 
     // The high 5 bits of the UTF-16 code unit are only nonzero for 3 and 4-byte
     // UTF-8 sequences.
-    p34b.createAssign(u16_hi[0], p34b.createOr(p34b.createAnd(u8scope33, bit4a2), surrogate));
-    p34b.createAssign(u16_hi[1], p34b.createOr(p34b.createAnd(u8scope33, bit5a2), surrogate));
-    p34b.createAssign(u16_hi[2], p34b.createAnd(u8scope33, bit6a2));
-    p34b.createAssign(u16_hi[3], p34b.createOr(p34b.createAnd(u8scope33, bit7a2), surrogate));
-    p34b.createAssign(u16_hi[4], p34b.createOr(p34b.createAnd(u8scope33, bit2a1), surrogate));
+    p34b.createAssign(u16_hi[7], p34b.createOr(p34b.createAnd(u8scope33, bit3a2), surrogate));
+    p34b.createAssign(u16_hi[6], p34b.createOr(p34b.createAnd(u8scope33, bit2a2), surrogate));
+    p34b.createAssign(u16_hi[5], p34b.createAnd(u8scope33, bit1a2));
+    p34b.createAssign(u16_hi[4], p34b.createOr(p34b.createAnd(u8scope33, bit0a2), surrogate));
+    p34b.createAssign(u16_hi[3], p34b.createOr(p34b.createAnd(u8scope33, bit5a1), surrogate));
 
     //
     nAb.createIf(pfx34, p34b);
@@ -232,34 +232,34 @@ void U8U16Kernel::generatePabloMethod() {
 
     // The low 3 bits of the high byte of the UTF-16 code unit as well as the high bit of the
     // low byte are only nonzero for 2, 3 and 4 byte sequences.
-    nAb.createAssign(u16_hi[5], nAb.createOr(nAb.createAnd(u8lastscope, bit3a1), u8scope44));
-    nAb.createAssign(u16_hi[6], nAb.createOr(nAb.createAnd(u8lastscope, bit4a1), s43_hi6));
-    nAb.createAssign(u16_hi[7], nAb.createOr(nAb.createAnd(u8lastscope, bit5a1), s43_hi7));
-    nAb.createAssign(u16_lo[0], nAb.createOr(nAb.createAnd(u8lastscope, bit6a1), s43_lo0));
+    nAb.createAssign(u16_hi[2], nAb.createOr(nAb.createAnd(u8lastscope, bit4a1), u8scope44));
+    nAb.createAssign(u16_hi[1], nAb.createOr(nAb.createAnd(u8lastscope, bit3a1), s43_hi1));
+    nAb.createAssign(u16_hi[0], nAb.createOr(nAb.createAnd(u8lastscope, bit2a1), s43_hi0));
+    nAb.createAssign(u16_lo[7], nAb.createOr(nAb.createAnd(u8lastscope, bit1a1), s43_lo7));
 
-    Var * p234_lo1 = main.createVar("p234_lo1", zeroes);
+    Var * p234_lo6 = main.createVar("p234_lo6", zeroes);
 
-    nAb.createAssign(p234_lo1, nAb.createOr(nAb.createAnd(u8lastscope, bit7a1), s43_lo1));
+    nAb.createAssign(p234_lo6, nAb.createOr(nAb.createAnd(u8lastscope, bit0a1), s43_lo6));
 
     main.createIf(nonASCII, nAb);
     //
     //
     PabloAST * ASCII = ccc.compileCC(re::makeByte(0x0, 0x7F));
     PabloAST * last_byte = main.createOr(ASCII, u8lastscope);
-    main.createAssign(u16_lo[1], main.createOr(main.createAnd(ASCII, u8_bits[1]), p234_lo1));
-    main.createAssign(u16_lo[2], main.createOr(main.createAnd(last_byte, u8_bits[2]), s43_lo2));
-    main.createAssign(u16_lo[3], main.createOr(main.createAnd(last_byte, u8_bits[3]), s43_lo3));
-    main.createAssign(u16_lo[4], main.createOr(main.createAnd(last_byte, u8_bits[4]), s43_lo4));
+    main.createAssign(u16_lo[6], main.createOr(main.createAnd(ASCII, u8_bits[6]), p234_lo6));
     main.createAssign(u16_lo[5], main.createOr(main.createAnd(last_byte, u8_bits[5]), s43_lo5));
-    main.createAssign(u16_lo[6], main.createOr(main.createAnd(last_byte, u8_bits[6]), s43_lo6));
-    main.createAssign(u16_lo[7], main.createOr(main.createAnd(last_byte, u8_bits[7]), s43_lo7));
+    main.createAssign(u16_lo[4], main.createOr(main.createAnd(last_byte, u8_bits[4]), s43_lo4));
+    main.createAssign(u16_lo[3], main.createOr(main.createAnd(last_byte, u8_bits[3]), s43_lo3));
+    main.createAssign(u16_lo[2], main.createOr(main.createAnd(last_byte, u8_bits[2]), s43_lo2));
+    main.createAssign(u16_lo[1], main.createOr(main.createAnd(last_byte, u8_bits[1]), s43_lo1));
+    main.createAssign(u16_lo[0], main.createOr(main.createAnd(last_byte, u8_bits[0]), s43_lo0));
 
     Var * output = getOutputStreamVar("u16bit");
     for (unsigned i = 0; i < 8; i++) {
-        main.createAssign(main.createExtract(output, i), u16_hi[i]);
+        main.createAssign(main.createExtract(output, i + 8), u16_hi[i]);
     }
     for (unsigned i = 0; i < 8; i++) {
-        main.createAssign(main.createExtract(output, i + 8), u16_lo[i]);
+        main.createAssign(main.createExtract(output, i), u16_lo[i]);
     }
     PabloAST * selectors = main.createInFile(main.createNot(delmask));
     main.createAssign(main.createExtract(getOutputStreamVar("selectors"), main.getInteger(0)), selectors);
@@ -278,7 +278,7 @@ u8u16FunctionType generatePipeline(CPUDriver & pxDriver) {
 
     // Transposed bits from s2p
     StreamSet * BasisBits = P->CreateStreamSet(8);
-    P->CreateKernelCall<S2PKernel>(ByteStream, BasisBits, cc::BitNumbering::BigEndian);
+    P->CreateKernelCall<S2PKernel>(ByteStream, BasisBits);
 
     // Calculate UTF-16 data bits through bitwise logic on u8-indexed streams.
     StreamSet * u8bits = P->CreateStreamSet(16);
@@ -300,10 +300,10 @@ u8u16FunctionType generatePipeline(CPUDriver & pxDriver) {
         P->CreateKernelCall<SwizzledDeleteByPEXTkernel>(selectors, u8bits, u16Swizzles);
         // Produce unswizzled UTF-16 bit streams
         P->CreateKernelCall<SwizzleGenerator>(u16Swizzles, std::vector<StreamSet *>{u16bits});
-        P->CreateKernelCall<P2S16Kernel>(u16bits, u16bytes, cc::BitNumbering::BigEndian);
+        P->CreateKernelCall<P2S16Kernel>(u16bits, u16bytes);
     } else {
         P->CreateKernelCall<FieldCompressKernel>(b->getBitBlockWidth()/16, u8bits, selectors, u16bits);
-        P->CreateKernelCall<P2S16KernelWithCompressedOutput>(u16bits, selectors, u16bytes, cc::BitNumbering::BigEndian);
+        P->CreateKernelCall<P2S16KernelWithCompressedOutput>(u16bits, selectors, u16bytes);
     }
 
     Scalar * outputFileName = P->getInputScalar("outputFileName");
