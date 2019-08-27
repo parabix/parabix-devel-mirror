c49b4d9b62bc9d128f1a36949b53de48b11c3d80
Rob Cameron <cameron@cs.sfu.ca>
2019-08-28 06:05:01 +0800

    More bit movement test cases

--- tests/test_bit_movement.cpp
+++ tests/test_bit_movement.cpp
@@ -26,9 +26,9 @@ TEST_CASE(insert_after1, insert_mask_i, insert_after_e) {
     AssertEQ(T, Result, Input<1>(T));
 }
 
-auto insert_counts = BinaryStreamSet({"00100010", "00100000", "10000000"});
-auto insert_mult_before_e = BinaryStream("0000110001111011");
-auto insert_mult_after_e = BinaryStream("1000011000111101");
+auto insert_counts = BinaryStreamSet({"001000101", "001000000", "100000000"});
+auto insert_mult_before_e = BinaryStream("000011000111101101");
+auto insert_mult_after_e = BinaryStream("100001100011110110");
 
 TEST_CASE(insert_mult_after, insert_counts, insert_mult_after_e) {
     auto Result = InsertionSpreadMask(T, Input<0>(T), InsertPosition::After);
@@ -40,9 +40,29 @@ TEST_CASE(insert_mult_before, insert_counts, insert_mult_before_e) {
     AssertEQ(T, Result, Input<1>(T));
 }
 
+auto to_filter = BinaryStreamSet({"01010101011", "11110000111", "11010011000"});
+auto filter_mask = BinaryStream({"11110000111"});
+auto filtered = BinaryStreamSet({"0101011", "1111111", "1101000"});
+
+TEST_CASE(filter1, filter_mask, to_filter, filtered) {
+    auto Result = T->CreateStreamSet(3);
+    FilterByMask(T, Input<0>(T), Input<1>(T), Result);
+    AssertEQ(T, Result, Input<2>(T));
+}
+
+auto spread = BinaryStreamSet({"01010000011", "11110000111", "11010000000"});
+
+TEST_CASE(spread1, filter_mask, filtered, spread) {
+    auto Result = T->CreateStreamSet(3);
+    SpreadByMask(T, Input<0>(T), Input<1>(T), Result);
+    AssertEQ(T, Result, Input<2>(T));
+}
+
 RUN_TESTS(
           CASE(insert_before1),
           CASE(insert_after1),
           CASE(insert_mult_after),
-          CASE(insert_mult_before)
+          CASE(insert_mult_before),
+          CASE(filter1),
+          CASE(spread1)
 )
