ad59a91fc394cff345b48ed6ec68cd37c7e553db
Rob Cameron <cameron@cs.sfu.ca>
2019-09-08 09:18:44 -0700

    Better oferflow test

--- tests/index_test.cpp
+++ tests/index_test.cpp
@@ -24,7 +24,6 @@ auto runs_i =       BinaryStream({"..111111.111111111.1..."});
 auto run_idx_e = BinaryStreamSet({"..010101.010101010.0...",
                                   "..001100.001100110.0...",
                                   "..000011.000011110.0..."});
-auto overflow_e =   BinaryStream({".................1....."});
 
 TEST_CASE(runs_1, runs_i, run_idx_e) {
     auto Result = T->CreateStreamSet(3);
@@ -32,8 +31,11 @@ TEST_CASE(runs_1, runs_i, run_idx_e) {
     AssertEQ(T, Result, Input<1>(T));
 }
 
+// Overflow when output streamset has 2 streams.
+auto overflow_e =   BinaryStream({"......11.....11111....."});
+
 TEST_CASE(runs_1overflow, runs_i, overflow_e) {
-    auto Result = T->CreateStreamSet(3);
+    auto Result = T->CreateStreamSet(2);
     auto OverFlow = T->CreateStreamSet(1);
     P->CreateKernelCall<RunIndex>(Input<0>(T), Result, OverFlow);
     AssertEQ(T, OverFlow, Input<1>(T));
