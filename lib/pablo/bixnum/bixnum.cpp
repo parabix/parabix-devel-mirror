423611c409e6298f6ba6338f4746c9a41510a85c
Rob Cameron <cameron@cs.sfu.ca>
2019-10-03 15:24:55 -0700

    Ensure that MulModular returns an output BixNum of the same size as the input BixNum

--- lib/pablo/bixnum/bixnum.cpp
+++ lib/pablo/bixnum/bixnum.cpp
@@ -236,11 +236,11 @@ BixNum BixNumCompiler::SubModular(BixNum minuend, BixNum subtrahend) {
 }
 
 BixNum BixNumCompiler::MulModular(BixNum multiplicand, unsigned multiplier) {
+    BixNum product(multiplicand.size(), mPB.createZeroes());
     if (multiplier == 0) {
-        return {mPB.createZeroes()};
+        return product;
     }
     unsigned multiplier_bits = std::log2(multiplier)+1;
-    BixNum product(multiplicand.size(), mPB.createZeroes());
     for (unsigned i = 0; i < multiplier_bits; i++) {
         if ((multiplier & (1 << i)) != 0) {
             PabloAST * carry = mPB.createZeroes();
@@ -249,7 +249,6 @@ BixNum BixNumCompiler::MulModular(BixNum multiplicand, unsigned multiplier) {
                 carry = mPB.createMajority3(product[j + i], multiplicand[j], carry);
                 product[j + i] = tmp;
             }
-            product[multiplicand.size() + i] = carry;
         }
     }
     return product;
