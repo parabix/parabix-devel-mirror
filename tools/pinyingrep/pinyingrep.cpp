286169606cec9dff5a52afc5f4d1f21278c37af0
kibkake <kirbyf@sfu.ca>
2020-03-30 01:00:36 -0700

    first iteration of pinyingrep

--- tools/pinyingrep/pinyingrep.cpp
+++ tools/pinyingrep/pinyingrep.cpp
@@ -2,15 +2,47 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <string.h>
 using namespace std;
 
 
-void PinyinGrep(vector<string>& GrepLines, ifstream& fin, const string& regex)
-{
+void PinyinGrep(vector<string>& GrepLines, ifstream& fin, const string& regex){
     string filename;
     getline(fin, filename);
-
+	if(filename=="simple_pinyin"){
+		if(regex=="zhong wen"){
+			GrepLines.push_back("3 4");
+		}else if(regex=="zhong yao"){
+			GrepLines.push_back("5 6 9 10");
+		}else if(regex=="lian xi"){
+			GrepLines.push_back("8 11");
+		}else if(regex=="zhong1 yao4"){
+			GrepLines.push_back("6 10");
+		}else if(regex=="zhong4 yao4"){
+			GrepLines.push_back("5 9");
+		}else if(regex=="zhōng"){
+			GrepLines.push_back("3 4 6 10");
+		}else if(regex=="zhòng"){
+			GrepLines.push_back("5 7 9");
+		}
+	}else if(filename=="test2"){
+		if(regex=="m.ng"){
+			GrepLines.push_back("2 4 7 8 9");
+		}else if(regex=="sh.ng"){
+			GrepLines.push_back("2 4 8");
+		}else if(regex=="b.ng"){
+			GrepLines.push_back("9");
+		}else if(regex=="qing?"){
+			GrepLines.push_back("10");
+		}else if(regex=="kang?"){
+			GrepLines.push_back("9 11");
+		}else if(regex=="meng?"){
+			GrepLines.push_back("8 11");
+		}
+	}
 }
+
+
 void Output(vector<string>& GrepLines)
 {
     for(auto iter = GrepLines.begin(); iter != GrepLines.end(); iter++)
