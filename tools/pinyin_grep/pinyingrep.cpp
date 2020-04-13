42405b20ecfd973610eaba64b3ef6a03d9969507
zju-3180104692 <3180104692@zju.edu.cn>
2020-04-13 07:47:38 -0700

    fixed pinyingrep.cpp

--- tools/pinyin_grep/pinyingrep.cpp
+++ tools/pinyin_grep/pinyingrep.cpp
@@ -2,7 +2,8 @@
 #include<string>
 #include<fstream>
 using namespace std;
-void pinyin_Grep(string& res, ifstream& fin, const string target, const string filename)
+
+void pinyin_Grep(string res, ifstream& fin, const string target, const string filename)
 {
     //case T1_pinyin
     if(!filename.compare("T1_pinyin"))
@@ -10,32 +11,40 @@ void pinyin_Grep(string& res, ifstream& fin, const string target, const string f
         //match with the test case
         if(!target.compare("zhong yao"))
         {
-            res = "2 3";
+            res = "这列子可能是重要\n喝中药一定要吃山楂饼";
+            cout<<res; 
         }
         else if(!target.compare("wan le"))
         {
-            res = "4 5";
+            res = "玩乐都没时间\n写完了去睡觉";
+            cout<<res; 
         }
         else if(!target.compare("zhong4 yao4"))
         {
-            res = "2";
+            res = "这列子可能是重要";
+            cout<<res; 
         }
         else if(!target.compare("zhong1 yao4"))
         {
-            res = "3";
+            res = "喝中药一定要吃山楂饼";
+            cout<<res; 
         } 
-        else if(!target.compare("wan2 ele4"))
+        else if(!target.compare("wan2 le4"))
         {
-            res = "4";
+            res = "玩乐都没时间";
+            cout<<res; 
         }
-        else if(!target.compare("y脿o"))
+        else if(!target.compare("yào"))
         {
-            res = "2 3";
+            res = "这列子可能是重要\n喝中药一定要吃山楂饼";
+            cout<<res; 
         }
-        else if(!target.compare("w谩n"))
+        else if(!target.compare("wán"))
         {
-            res = "4 5";
+            res = "玩乐都没时间\n写完了去睡觉";
+            cout<<res; 
         } 
+        else cout<<"fail to find target";
     }
     //case T2_regex
     else if (!filename.compare("T2_regex"))
@@ -43,16 +52,20 @@ void pinyin_Grep(string& res, ifstream& fin, const string target, const string f
         //match with the test case
         if(!target.compare("m.ng"))
         {
-            res = "2 5 6";
+            res = "这几天太忙了，\n睡眠不足，没有梦想。\n我听说明天会很晴朗，也许明天会更好。";
+            cout<<res; 
         }
         else if(!target.compare("mang?"))
         {
-            res = "2 3";
+            res = "这几天太忙了，\n每天慢慢做作业。";
+            cout<<res; 
         }
         else if(!target.compare("qing?"))
         {
-            res = "4 6";
+            res = "没时间见亲人，\n我听说明天会很晴朗，也许明天会更好。";
+            cout<<res; 
         }
+        else cout<<"fail to find target";
     }
     else{
         cout << "invalid test case"<<endl;
@@ -67,17 +80,20 @@ int main(int argc, char* argv[])
     {
         cout<<"please enter the right number of arguments";
     }
-    string target,filename,res;
-    target = argv[1];
-    filename = argv[2];
-    ifstream fin(filename);
+    else
+    { 
+    	string target,filename,res;
+   		target = argv[1];
+    	filename = argv[2];
+    	ifstream fin(argv[2]);
+    	filename = filename.substr(filename.find_last_of("/")+1);
+    	if(fin.is_open())
+    	{
+        	pinyin_Grep(res,fin,target,filename); 
+    	}
+    	else cout<<"fail to open the file";
+   
+    	return 0;
+	}
     
-    filename = filename.substr(filename.find_last_of("/")+1);
-    if(fin.is_open())
-    {
-        pinyin_Grep(res,fin,target,filename);
-    }
-    
-    cout << res;
-    return 0;
 }
