5f0caca383e63588f6a3d27a8ad04bce8b5e5def
zju-3180101968 <3180101968@zju.edu.cn>
2020-04-25 23:02:21 +0800

    Pinyin Interface Frame

---
+++ tools/wc/pinyin_count/pinyin_interface.cpp
@@ -0,0 +1,21 @@
+#include "pinyin_interface.h"
+
+using namespace std;
+namespace PY{
+    // Methods in PinyinValuesParser
+    void PinyinValuesParser::parse(string toparse){
+        
+    }
+
+    // Methods in PinyinValuesEnumerator
+    void PinyinValuesEnumerator::enumerate(PinyinValuesParser& parser){
+
+    }
+
+    // Methods in PinyinValuesTable
+    string PinyinValuesTable::get_intial(string s){
+
+    }
+    // ...
+
+}
