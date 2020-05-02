075a621768beca328ee5302f1628becb34aa056f
aytang <aytang@sfu.ca>
2020-05-02 14:58:51 -0700

    Small changes in import headers for krskagnxi.h

--- unihan-scripts/unihan_properties.py
+++ unihan-scripts/unihan_properties.py
@@ -3,12 +3,14 @@
 # and generating C headers for property data using a compact bitset
 # representation.
 #
+
 import string, os.path, os
 from unihan_parser import *
 
 def emit_enumerated_property(f, property_code, independent_prop_values, prop_values, value_map):
     f.write("  namespace %s_ns {\n" % property_code.upper())
     f.write("    const unsigned independent_prop_values = %s;\n" % independent_prop_values)
+
     for v in prop_values:
         f.write("    /** Code Point Ranges for %s\n    " % v)
         f.write(cformat.multiline_fill(['[%04x, %04x]' % (lo, hi) for (lo, hi) in uset_to_range_list(value_map[v])], ',', 4))
@@ -26,7 +28,7 @@ def get_property_full_name(property_code):
     if(property_code == "krs"):
         property_name = "kRSKangXi"
     return property_name
-    
+
 class unihan_generator():
     def __init__(self):
         self.parsed_map = []
@@ -34,19 +36,16 @@ class unihan_generator():
         self.property_data_headers = []
 
     def emit_property(self, f, property_code, prop_values, independent_prop_values, value_map):
-    
         prop_full_name = get_property_full_name(property_code)
-        
         emit_enumerated_property(f, property_code, independent_prop_values, prop_values, value_map)
         print("%s: %s bytes" % (prop_full_name, sum_bytes(value_map)))
-        
         self.supported_props.append(property_code)
     
     def generate_property_value_file(self, filename_root, property_code):
         prop_values, independent_prop_values, value_map = parse_property_file(filename_root, property_code)
         property_name = get_property_full_name(property_code)
         f = cformat.open_header_file_for_write(property_name)
-        cformat.write_imports(f, ['"PropertyAliases.h"', '"PropertyObjects.h"', '"PropertyValueAliases.h"', '"unicode_set.h"'])
+        cformat.write_imports(f, ['"PropertyAliases.h"', '"PropertyObjects.h"', '"PropertyValueAliases.h"', '<unicode/core/unicode_set.h>'])
         f.write("\nnamespace UCD {\n")
         self.emit_property(f, property_code, prop_values, independent_prop_values, value_map)
         f.write("}\n")
