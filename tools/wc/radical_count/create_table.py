ac9335c0e969299e13a5ad2bc088893f4491e758
zju-3180103307 <3180103307@zju.edu.cn>
2020-05-07 14:04:46 +0800

    radical count

---
+++ tools/wc/radical_count/create_table.py
@@ -0,0 +1,34 @@
+fr = open("unicodeset_radical_table.txt", "w+")
+
+lines = []
+krskangxi_path = '/Users/yuruonan/Downloads/parabix-devel/include/unicode/data/kRSKangXi.h'
+f = open(krskangxi_path, "r")
+
+for l in f:
+    lines.append(l)
+    
+table = []
+
+for i in range(1, 215):
+    code = str(i)
+    search = "    const static UnicodeSet::run_t __" + "_" + code + "_Set_runs[] = {\n"
+    if search in lines:
+        temp = ("{" + code + ",&_" + code + "_Set}")  #{74,&_74_Set}
+        temp = temp + ","
+        table.append(temp.ljust(35))
+        
+table[-1] = table[-1].strip(' ')
+table[-1] = table[-1][:-1]
+
+c=0
+for i in table:
+    if i == table[-1]:
+        fr.write(i)
+        continue
+    fr.write(i)
+    c = c + 1
+    if (c >= 6):
+        fr.write('\n')
+        c = 0
+
+fr.close()
---
+++ tools/wc/radical_count/radical_interface.h
@@ -0,0 +1,36 @@
+#ifndef radical_interface_h
+#define radical_interface_h
+
+#include <vector>
+#include <string>
+#include <array>
+#include <map>
+#include <utility>
+#include <set>
+#include <regex>
+#include <iostream>
+#include <unicode/data/kRSKangXi.h>
+
+namespace BS
+{
+    using std::vector;
+    using std::array;
+    using std::string;
+    using std::map;
+    using std::set;
+
+    class UnicodeSetTable
+    {
+        const UCD::UnicodeSet&& get_uset(string radical)
+        {
+            if(_unicodeset_radical_table.find(radical) != _unicodeset_radical_table.end())
+                return std::move(*_unicodeset_radical_table[radical]);
+            else
+                return std::move(UCD::UnicodeSet());
+        }
+        private:
+        static <string, const UCD::UnicodeSet*> _unicodeset_radical_table;
+    };
+}
+
+#endif /* radical_interface_h */
