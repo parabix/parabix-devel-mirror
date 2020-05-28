d54fbb7d0560fc6583088a28bff617c28afbc152
jd <jaddie_tan@sfu.ca>
2020-05-28 03:19:35 -0700

    pinyin parsing

--- tools/pinyin_grep/pinyin.h
+++ tools/pinyin_grep/pinyin.h
@@ -67,7 +67,8 @@ namespace PinyinPattern{
     {
         int pos = 0;
         Pinyin_syllables = trim(Pinyin_syllables);
-        vector<string> Divided;
+        vector<string> Divided, FinalVec;
+        string temp;
         while(pos!=-1)
         {
             string temp;
@@ -85,13 +86,79 @@ namespace PinyinPattern{
         //Task1
         //one function need to be here to modify all the syllables as a normal form as hōng
         //for example hong1 to hōng and hong to hōng......etc
-        
+
+        vector <vector<string>> syl { {"a", "ā", "á", "ǎ", "à"}, 
+                                    {"o", "ō", "ó", "ǒ", "ò"},
+                                    {"e", "ē", "é", "ě", "è"},
+                                    {"i", "ī", "í", "ǐ", "ì"},
+                                    {"u", "ū", "ú", "ǔ", "ù"},
+                                    {"ê", "ê̄ ", "ế ", "ê̌ ", "ề "},
+                                    {"m", "m̄", "ḿ", "m̀"},
+                                    {"n", "ń", "ň", "ǹ"} };
+        int pos = 0;
+        for(int i=0; i<Divided.size(); i++){
+            string word = Divided[i];
+            if (word.find('.') != string::npos) // found
+            {   
+                for (int j=0; j<6; j++){
+                    temp = word;
+                    temp.replace(word.find('.'), 1, syl[j][0]);
+                    Divided.push_back(temp);
+                }
+                Divided.erase(Divided.begin()+pos);
+                i--;
+            }
+            else if (word.find('1') != string::npos || word.find('2') != string::npos|| word.find('3') != string::npos|| word.find('4') != string::npos)
+            {
+                int tone;
+                if (word.find('1') != string::npos)
+                {
+                    tone = 1;
+                }
+                else if (word.find('2') != string::npos)
+                {
+                    tone = 2;
+                }
+                else if (word.find('3') != string::npos)
+                {
+                    tone = 3;
+                }
+                else if (word.find('4') != string::npos)
+                {
+                    tone = 4;
+                }
+                word.replace(word.find(to_string(tone)), 1, "");
+                for (int j=0; j<syl.size(); j++){
+                    if (word.find(syl[j][0]) != string::npos){
+                        word.replace(word.find(syl[j][0]), 1, syl[j][tone]);
+                        FinalVec.push_back(word);
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                for (int j=0; j<syl.size(); j++)
+                {
+                    if (Divided[i].find(syl[j][0]) != string::npos){
+                        for (int k=1; k<syl[j].size(); k++){
+                            temp = word;
+                            temp.replace(word.find(syl[j][0]), 1, syl[j][k]);
+                            FinalVec.push_back(temp);;
+                        }
+                        break;
+                    }
+                }
+            }
+            pos++;
+        }
+
         //adding the kHanyuPinyin.* to all the strings
-        for(vector<string>::iterator iter = Divided.begin(); iter!=Divided.end(); iter++)
+        for(vector<string>::iterator iter = FinalVec.begin(); iter!=FinalVec.end(); iter++)
         {
             *iter = Add_Search_Prefix(*iter);
         }
-        return Divided;
+        return FinalVec;
     }
     class Buffer{
         size_t rmd;
@@ -147,7 +214,7 @@ namespace PinyinPattern{
         }
     };
 
-        class PinyinSetAccumulator : public grep::MatchAccumulator {
+    class PinyinSetAccumulator : public grep::MatchAccumulator {
         UCD::UnicodeSet mAccumSet;
         unsigned int parsed_codepoint;
         string strcodepoint;
