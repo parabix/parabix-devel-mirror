6f59102adf8c8c84e44dd42089d270c5d9cabac8
钱闻博 <kevinq@qianwenbodeMacBook-Pro.local>
2020-06-06 16:05:32 +0800

    optimized the datafile and canceled the comma sperate and support of quotes

---
+++ tools/pinyin_grep/pinyin.cpp
@@ -0,0 +1,153 @@
+#include"pinyin.h"
+namespace PinyinPattern{
+/*  
+    Before Search works as the following three steps:
+    1. divide the input into single or multi syllables1
+    2. parse those syllables to transfer them to formal format for searching
+    3. add search prefix and suffix
+*/
+    vector<string> Before_Search(string Pinyin_syllables)
+    {
+        vector<string> Divided, FinalVec;
+        int pos = 0;
+        Pinyin_syllables = trim(Pinyin_syllables);
+        
+        string temp, temp2;
+        while(pos!=-1)
+        {
+            string temp;
+            pos = Pinyin_syllables.find_first_of(' ',pos);
+            temp = Pinyin_syllables.substr(0,pos);
+            Pinyin_syllables.erase(0,pos);
+            temp = trim(temp);
+            Divided.push_back(temp);
+            Pinyin_syllables = trim(Pinyin_syllables);
+        }
+        if(Pinyin_syllables!="")
+        {
+            Divided.push_back(Pinyin_syllables);
+        }
+        
+
+        vector <vector<string>> syl { {"a", "ā", "á", "ǎ", "à"}, 
+                                    {"o", "ō", "ó", "ǒ", "ò"},
+                                    {"e", "ē", "é", "ě", "è"},
+                                    {"i", "ī", "í", "ǐ", "ì"},
+                                    {"u", "ū", "ú", "ǔ", "ù"},
+                                    {"ê", "ê̄ ", "ế ", "ê̌ ", "ề "},
+                                    {"m", "m̄", "ḿ", "m̀"},
+                                    {"n", "ń", "ň", "ǹ"} };
+        for(size_t i=0; i<Divided.size(); i++){
+            string word = Divided[i];
+            if (word.find('.') != string::npos) // found regex . 
+            {   
+                for (int j=0; j<6; j++){
+                    temp = word;
+                    temp.replace(word.find('.'), 1, syl[j][0]);
+                    Divided.push_back(temp);
+                }
+                Divided.erase(Divided.begin()+i);
+                i--;
+            }
+            else if (word.find('?') != string::npos) // found regex ?
+            {
+                temp = temp2 = word;
+                temp.replace(word.find('?'), 1, "");
+                Divided.push_back(temp);
+                temp2.replace(word.find('?')-1, 2, "");
+                Divided.push_back(temp2);
+            }
+            else if (word.find('1') != string::npos || word.find('2') != string::npos|| word.find('3') != string::npos|| word.find('4') != string::npos)
+            {
+                int tone;
+                if (word.find('1') != string::npos)
+                {
+                    tone = 1;
+                }
+                else if (word.find('2') != string::npos)
+                {
+                    tone = 2;
+                }
+                else if (word.find('3') != string::npos)
+                {
+                    tone = 3;
+                }
+                else if (word.find('4') != string::npos)
+                {
+                    tone = 4;
+                }
+                word.replace(word.find(to_string(tone)), 1, "");
+                for (size_t j=0; j<syl.size(); j++){
+                    if (word.find(syl[j][0]) != string::npos){
+                        word.replace(word.find(syl[j][0]), 1, syl[j][tone]);
+                        FinalVec.push_back(word);
+                        break;
+                    }
+                }
+            }
+            else if (All_Alpha(word))
+            {
+                for (size_t j=0; j<syl.size(); j++)
+                {
+                    if (Divided[i].find(syl[j][0]) != string::npos){
+                        for (size_t k=1; k<syl[j].size(); k++){
+                            temp = word;
+                            temp.replace(word.find(syl[j][0]), 1, syl[j][k]);
+                            FinalVec.push_back(temp);;
+                        }
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                FinalVec.push_back(word);
+            }
+            
+        }
+        Divided = vector <string>(); //deallocate unused vector 
+
+        //adding the prefix and suffix to all the strings
+        for(vector<string>::iterator iter = FinalVec.begin(); iter!=FinalVec.end(); iter++)
+        {
+            *iter = Add_Search_Prefix(*iter);
+        }
+        // for (auto x: FinalVec){
+        //     llvm::errs() << x << " ";
+        // }
+        // llvm::errs() << "\n";
+        return FinalVec;
+    }
+    // trim erases the extra space from both head and tail of a std::string
+    string& trim(string &s)
+    {
+        if(s.empty())
+        {
+            return s;
+        }
+        
+        s.erase(0,s.find_first_not_of(" "));
+        s.erase(s.find_last_not_of(" ") + 1);
+        return s;
+    }
+    // Add_Search_Prefix adds both the prefix and suffix of the search module to search in the grep engine
+    string Add_Search_Prefix(string to_add)
+    {
+        string temp = to_add;
+        string prefix = "(kHanyuPinyin.*"; 
+        string suffix = ")(,|$)";
+        temp = prefix + temp + suffix;
+        return temp;
+    }
+    
+
+    bool All_Alpha(string word)
+    {
+        for(size_t a=0; a<word.length(); a++){
+            if (!isalpha(word[a])){
+                return false;
+            }
+        }
+        return true;
+    }
+}
--- tools/pinyin_grep/pinyin.h
+++ tools/pinyin_grep/pinyin.h
@@ -23,182 +23,7 @@
 
 namespace PinyinPattern{
     using namespace std;
-    class InputChecker{
-        bool is_check;
-        vector<string> first_check;
-        vector <pair<string, int> >final;
-        public:
-        InputChecker(){is_check = false;first_check.clear();}
-        InputChecker(string s)
-        {
-            InputCheck(s);
-            is_check = true;
-        }
-        void InputCheck(string to_check){
-            MultiInput_Check(to_check,first_check);
-            Divide();
-            is_check = true;
-        }
-        void MultiInput_Check(string to_check, vector<string> &multiCheck_store);
-        string& trim(string &s) 
-        {
-            if (s.empty()) 
-            {
-                return s;
-            }
-            s.erase(0,s.find_first_not_of(" "));
-            s.erase(s.find_last_not_of(" ") + 1);
-            return s;
-        }
-        void Divide();
-    };
-    string& trim(string &s)
-    {
-        if(s.empty())
-        {
-            return s;
-        }
-        
-        s.erase(0,s.find_first_not_of(" "));
-        s.erase(0,s.find_first_not_of(","));
-        s.erase(s.find_last_not_of(" ") + 1);
-        return s;
-    }
-    string Add_Search_Prefix(string to_add)
-    {
-        string temp = to_add;
-        string prefix = "(kHanyuPinyin.*"; 
-        string suffix = ")(,|$)";
-        temp = prefix + temp + suffix;
-        // string prefix = "kHanyu.*";
-        // temp = prefix + temp;
-        return temp;
-    }
-    //this function should be rewriten totally inorder to deal with multiple syllables since it has no space between two syllables
-    //should find a new way to divided the syllables by not using the space
-    bool All_Alpha(string word)
-    {
-        for(size_t a=0; a<word.length(); a++){
-            if (!isalpha(word[a])){
-                return false;
-            }
-        }
-        return true;
-    }
-    vector<string> Before_Search(string Pinyin_syllables)
-    {
-        vector<string> Divided, FinalVec;
-        int pos = 0;
-        Pinyin_syllables = trim(Pinyin_syllables);
-        
-        string temp, temp2;
-        while(pos!=-1)
-        {
-            string temp;
-            pos = Pinyin_syllables.find_first_of(',',pos);
-            temp = Pinyin_syllables.substr(0,pos);
-            Pinyin_syllables.erase(0,pos);
-            temp = trim(temp);
-            Divided.push_back(temp);
-            Pinyin_syllables = trim(Pinyin_syllables);
-        }
-        if(Pinyin_syllables!="")
-        {
-            Divided.push_back(Pinyin_syllables);
-        }
-        //Task1
-        //one function need to be here to modify all the syllables as a normal form as hōng
-        //for example hong1 to hōng and hong to hōng......etc
-
-        vector <vector<string>> syl { {"a", "ā", "á", "ǎ", "à"}, 
-                                    {"o", "ō", "ó", "ǒ", "ò"},
-                                    {"e", "ē", "é", "ě", "è"},
-                                    {"i", "ī", "í", "ǐ", "ì"},
-                                    {"u", "ū", "ú", "ǔ", "ù"},
-                                    {"ê", "ê̄ ", "ế ", "ê̌ ", "ề "},
-                                    {"m", "m̄", "ḿ", "m̀"},
-                                    {"n", "ń", "ň", "ǹ"} };
-        for(size_t i=0; i<Divided.size(); i++){
-            string word = Divided[i];
-            if (word.find('.') != string::npos) // found regex . 
-            {   
-                for (int j=0; j<6; j++){
-                    temp = word;
-                    temp.replace(word.find('.'), 1, syl[j][0]);
-                    Divided.push_back(temp);
-                }
-                Divided.erase(Divided.begin()+i);
-                i--;
-            }
-            else if (word.find('?') != string::npos) // found regex ?
-            {
-                temp = temp2 = word;
-                temp.replace(word.find('?'), 1, "");
-                Divided.push_back(temp);
-                temp2.replace(word.find('?')-1, 2, "");
-                Divided.push_back(temp2);
-            }
-            else if (word.find('1') != string::npos || word.find('2') != string::npos|| word.find('3') != string::npos|| word.find('4') != string::npos)
-            {
-                int tone;
-                if (word.find('1') != string::npos)
-                {
-                    tone = 1;
-                }
-                else if (word.find('2') != string::npos)
-                {
-                    tone = 2;
-                }
-                else if (word.find('3') != string::npos)
-                {
-                    tone = 3;
-                }
-                else if (word.find('4') != string::npos)
-                {
-                    tone = 4;
-                }
-                word.replace(word.find(to_string(tone)), 1, "");
-                for (size_t j=0; j<syl.size(); j++){
-                    if (word.find(syl[j][0]) != string::npos){
-                        word.replace(word.find(syl[j][0]), 1, syl[j][tone]);
-                        FinalVec.push_back(word);
-                        break;
-                    }
-                }
-            }
-            else if (All_Alpha(word))
-            {
-                for (size_t j=0; j<syl.size(); j++)
-                {
-                    if (Divided[i].find(syl[j][0]) != string::npos){
-                        for (size_t k=1; k<syl[j].size(); k++){
-                            temp = word;
-                            temp.replace(word.find(syl[j][0]), 1, syl[j][k]);
-                            FinalVec.push_back(temp);;
-                        }
-                        break;
-                    }
-                }
-            }
-            else
-            {
-                FinalVec.push_back(word);
-            }
-            
-        }
-        Divided = vector <string>(); //deallocate unused vector 
-
-        //adding the kHanyuPinyin.* to all the strings
-        for(vector<string>::iterator iter = FinalVec.begin(); iter!=FinalVec.end(); iter++)
-        {
-            *iter = Add_Search_Prefix(*iter);
-        }
-        // for (auto x: FinalVec){
-        //     llvm::errs() << x << " ";
-        // }
-        // llvm::errs() << "\n";
-        return FinalVec;
-    }
+    
     class Buffer{
         size_t rmd;
         size_t size, size32, diff;
@@ -252,7 +77,7 @@ namespace PinyinPattern{
             return fstring;
         }
     };
-
+    
     class PinyinSetAccumulator : public grep::MatchAccumulator {
         UCD::UnicodeSet mAccumSet;
         unsigned int parsed_codepoint;
@@ -264,7 +89,7 @@ namespace PinyinPattern{
             s >> ret;
             return ret;
         }
-        public:
+    public:
         PinyinSetAccumulator() {}
         UCD::UnicodeSet && getAccumulatedSet() {
             return move(mAccumSet);
@@ -284,5 +109,10 @@ namespace PinyinPattern{
             mAccumSet.insert(parsed_codepoint);
         }
     };
+    vector<string> Before_Search(string Pinyin_syllables);
+    string& trim(string &s);
+    string Add_Search_Prefix(string to_add);
+    bool All_Alpha(string word);
+    
 }
 #endif
