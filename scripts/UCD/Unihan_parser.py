63f160c86726a3cf3ede3102b4f17b488145553d
Rob Cameron <cameron@cs.sfu.ca>
2021-02-26 17:32:40 -0800

    Support for UnicodeSets with a given CJK Radical

--- scripts/UCD/Unihan_parser.py
+++ scripts/UCD/Unihan_parser.py
@@ -7,7 +7,7 @@
 # Licensed under Open Software License 3.0.
 #
 #
-import UCD_config, re
+import UCD_config, re, cformat
 from unicode_set import *
 
 #
@@ -58,6 +58,13 @@ def generate_Radical_sets():
 
 CJKradicals_regexp = re.compile("^([0-9]{1,3}'?);\s+([0-9A-F]{4,6});\s+([0-9A-F]{4,6})\s*$")
 
+
+def radicalIdent(radicalCode):
+    radical_id = radicalCode
+    if radicalCode[-1] == "'":
+        radical_id = radical_id[:-1] + "s"
+    return "kangXi_" + radical_id
+
 def parse_CJK_Radicals_txt():
     f = open(UCD_config.UCD_src_dir + "/CJKRadicals.txt")
     lines = f.readlines()
@@ -76,18 +83,49 @@ def parse_CJK_Radicals_txt():
         ideograph_map[radical] = ideograph_cp
     return (radical_map, ideograph_map)
 
+def generate_Radical_Set_Map():
+    (radical_map, ideograph_map) = parse_CJK_Radicals_txt()
+    gen = ""
+    pending = "\n    {"
+    for radical_code in radical_map.keys():
+        cps = [radical_map[radical_code], ideograph_map[radical_code]]
+        r = "&" + radicalIdent(radical_code)
+        for cp in cps:
+            gen += pending + "{0x%x, %s}" % (cp, r)
+            pending = ",\n    "
+    gen += "};\n"
+    return gen
+
+RadicalSets_cpp_template = r"""#include <map>
+#include <unicode/data/RadicalSets.h>
+#include <unicode/core/unicode_set.h>
+
+namespace UCD {
+
+%s
+
+static std::map<codepoint_t, const UnicodeSet *> RadicalSetMap = %s;
+
+const UnicodeSet * getRadicalSet(codepoint_t radical_cp) {
+    auto f = RadicalSetMap.find(radical_cp);
+    if (f == RadicalSetMap.end()) return nullptr;
+    return f->second;
+}
+}
+"""
+
+def generate_RadicalSets_cpp():
+    f = cformat.open_cpp_file_for_write('RadicalSets')
+    radical_sets = generate_Radical_sets()
+    set_decls = ""
+    for radical in sorted(radical_sets.keys()):
+        set_decls += radical_sets[radical].generate(radicalIdent(radical))
+    radical_set_map = generate_Radical_Set_Map()
+    f.write(RadicalSets_cpp_template % (set_decls, radical_set_map))
+    f.close()
 
 def Unihan_main():
-    radical_map = generate_Radical_sets()
-    for radical in sorted(radical_map.keys()):
-        radical_id = radical
-        if radical[-1] == "'":
-            radical_id = radical[:-2] + "s"
-        print(radical_map[radical].generate("kangXi" + radical_id))
-    (radical_map, ideograph_map) = parse_CJK_Radicals_txt()
-    print(radical_map)
-    print(ideograph_map)
-
+    generate_RadicalSets_cpp()
 
 if __name__ == "__main__":
   Unihan_main()
